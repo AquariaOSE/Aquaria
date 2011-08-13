0951283b31b9f2c191dcfb7ec917cbadf127658b
fgenesis <false.genesis@googlemail.com>
2011-08-13 14:50:47 +0200

    move around some files to keep the size of the next commit down

--- Aquaria/UserSettings.cpp
+++ Aquaria/UserSettings.cpp
@@ -25,7 +25,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 	#include "Game.h"
 	#include "Avatar.h"
 #else
-	#include "../ExternalLibs/tinyxml.h"
+	#include "tinyxml.h"
 #endif
 
 #ifdef BBGE_BUILD_WINDOWS
