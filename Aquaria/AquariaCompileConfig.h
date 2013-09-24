ded3cb8c08d14ca32757063df652a8c770cbd739
fgenesis <false.genesis@googlemail.com>
2013-09-24 17:25:33 +0200

    Display build date in version string.

--- Aquaria/AquariaCompileConfig.h
+++ Aquaria/AquariaCompileConfig.h
@@ -9,7 +9,7 @@
     #define AQUARIA_BUILD_CONSOLE 1
     #define AQUARIA_BUILD_SCENEEDITOR 1
 
-    #define AQUARIA_CUSTOM_BUILD_ID ""
+    #define AQUARIA_CUSTOM_BUILD_ID (" Build " __DATE__ " - " __TIME__)
 
     // If defined, this is shown instead of "Aquaria vx.x.x ..." on the title screen.
     //#define AQUARIA_OVERRIDE_VERSION_STRING "Aquaria OSE v1.000"
