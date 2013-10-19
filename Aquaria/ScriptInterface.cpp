d97bb25b9e57f278598d15d384b493d7320185cd
fgenesis <false.genesis@googlemail.com>
2013-10-19 17:30:42 +0200

    Add some Lua functions, changes to hair.
    + beam_getEndPos()
    + entity_setCurrentHealth()
    + entity_setMaxHealth()
    + beam_getEndPos()
    + entity_hasAnimation()
    
    - Add 3 new parameters to entity_damage: shot, hitX, hitY
    - entity_getAnimationLength() supports string as 2nd parameter now.
      This will lookup an animation's length via name.
      Passing an ANIMLAYER_* constant works as it used to.

--- Aquaria/ScriptInterface.cpp
+++ Aquaria/ScriptInterface.cpp
@@ -3815,15 +3815,27 @@ luaFunc(entity_getAnimationLength)
 {
 	Entity *e = entity(L);
 	float ret=0;
-	int layer = lua_tonumber(L, 2);
 	if (e)
 	{
-		if (Animation *anim = e->skeletalSprite.getCurrentAnimation(layer))
+		Animation *anim = 0;
+		if (lua_isstring(L, 2))
+			anim = e->skeletalSprite.getAnimation(lua_tostring(L, 2));
+		else
 		{
-			ret = anim->getAnimationLength();
+			int layer = lua_tointeger(L, 2);
+			anim = e->skeletalSprite.getCurrentAnimation(layer);
 		}
+		if (anim)
+			ret = anim->getAnimationLength();
 	}
 	luaReturnNum(ret);
+}
+
+luaFunc(entity_hasAnimation)
+{
+	Entity *e = entity(L);
+	Animation *anim = e->skeletalSprite.getAnimation(getString(L, 2));
+	luaReturnBool(anim != NULL);
 }
 
 luaFunc(entity_isFollowingPath)
@@ -4233,6 +4245,15 @@ luaFunc(beam_setPosition_override)
 	luaReturnNil();
 }
 
+luaFunc(beam_getEndPos)
+{
+	Beam *b = beam(L);
+	Vector v;
+	if (b)
+		v = b->endPos;
+	luaReturnVec2(v.x, v.y);
+}
+
 luaFunc(getStringBank)
 {
 	luaReturnStr(dsq->continuity.stringBank.get(lua_tointeger(L, 1)).c_str());
@@ -4411,6 +4432,8 @@ luaFunc(entity_damage)
 		d.damageType = (DamageType)lua_tointeger(L, 4);
 		d.effectTime = lua_tonumber(L, 5);
 		d.useTimer = !getBool(L, 6);
+		d.shot = lua_isuserdata(L, 7) ? getShot(L, 7) : NULL;
+		d.hitPos = Vector(lua_tonumber(L, 8), lua_tonumber(L, 9));
 		didDamage = e->damage(d);
 	}
 	luaReturnBool(didDamage);
@@ -4445,6 +4468,22 @@ luaFunc(entity_setHealth)
 	Entity *e = entity(L, 1);
 	if (e)
 		e->health = e->maxHealth = lua_tonumber(L, 2);
+	luaReturnNil();
+}
+
+luaFunc(entity_setCurrentHealth)
+{
+	Entity *e = entity(L, 1);
+	if (e)
+		e->health = lua_tonumber(L, 2);
+	luaReturnNil();
+}
+
+luaFunc(entity_setMaxHealth)
+{
+	Entity *e = entity(L, 1);
+	if (e)
+		e->maxHealth = lua_tonumber(L, 2);
 	luaReturnNil();
 }
 
@@ -8716,6 +8755,7 @@ static const struct {
 	luaRegister(beam_setBeamWidth),
 	luaRegister(beam_setFirer),
 	luaRegister(beam_setDamageType),
+	luaRegister(beam_getEndPos),
 
 	luaRegister(getStringBank),
 
@@ -9297,6 +9337,8 @@ static const struct {
 
 
 	luaRegister(entity_setHealth),
+	luaRegister(entity_setCurrentHealth),
+	luaRegister(entity_setMaxHealth),
 	luaRegister(entity_changeHealth),
 
 	luaRegister(node_setActive),
@@ -9325,6 +9367,7 @@ static const struct {
 	luaRegister(entity_isAnimating),
 	luaRegister(entity_getAnimationName),
 	luaRegister(entity_getAnimationLength),
+	luaRegister(entity_hasAnimation),
 
 	luaRegister(entity_setCull),
 
