1c61dbb1c56d9276019822664409c43f24865ad8
fgenesis <false.genesis@googlemail.com>
2014-04-07 02:10:05 +0200

    Update Aquaria/BBGE/External sources to comply with the new ttvfs API

--- BBGE/Core.cpp
+++ BBGE/Core.cpp
@@ -5136,33 +5136,21 @@ void Core::setupFileAccess()
 	if(!ttvfs::checkCompat())
 		exit_error("ttvfs not compatible");
 
+	ttvfs_setroot(&vfs);
+
+	vfs.AddLoader(new ttvfs::DiskLoader);
 	vfs.AddArchiveLoader(new ttvfs::VFSZipArchiveLoader);
 
-	if(!vfs.LoadFileSysRoot(false))
-	{
-		exit_error("Failed to setup file access");
-	}
-	
-	vfs.Prepare();
-
-
-	ttvfs::VFSDir *override = vfs.GetDir("override");
-	if(override)
-	{
-		debugLog("Mounting override dir...");
-		override->load(true);
-		vfs.Mount("override", "", true);
-	}
+	vfs.Mount("override", "");
 
 	// If we ever want to read from a container...
-	//vfs.AddArchive("aqfiles.zip", false, "");
+	//vfs.AddArchive("aqfiles.zip");
 
 	if(_extraDataDir.length())
 	{
 		debugLog("Mounting extra data dir: " + _extraDataDir);
-		vfs.MountExternalPath(_extraDataDir.c_str(), "", true, true);
+		vfs.Mount(_extraDataDir.c_str(), "");
 	}
-
 
 	debugLog("Done");
 #endif
