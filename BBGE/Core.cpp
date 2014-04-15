e51164106764907fe3413b0eb880cb1af5b05e23
fgenesis <false.genesis@googlemail.com>
2014-04-15 19:48:06 +0200

    ttvfs related cleanups

--- BBGE/Core.cpp
+++ BBGE/Core.cpp
@@ -57,6 +57,10 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 	Vector unchange;
 #endif
 
+#ifdef BBGE_BUILD_VFS
+#include "ttvfs.h"
+#endif
+
 Core *core = 0;
 
 #ifdef BBGE_BUILD_WINDOWS
