26dc8560b78ccce7e1faf82edcc7bf533fcf2296
fgenesis <false.genesis@googlemail.com>
2014-09-15 20:59:21 +0200

    Fix gcc compile (broke in 300f3267776)

--- BBGE/RenderObject.cpp
+++ BBGE/RenderObject.cpp
@@ -23,6 +23,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "MathFunctions.h"
 
 #include <assert.h>
+#include <algorithm>
 
 bool	RenderObject::renderCollisionShape			= false;
 int		RenderObject::lastTextureApplied			= 0;
