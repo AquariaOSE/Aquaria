ae61d646b013fa575d60515f7d88e912411815cc
fgenesis <false.genesis@googlemail.com>
2016-07-18 03:21:24 +0200

    Fix startup crash and linking error in release mode

--- BBGE/StateManager.h
+++ BBGE/StateManager.h
@@ -22,6 +22,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #define __state_data__
 
 #include "Base.h"
+#include <map>
 #include "ActionMapper.h"
 #include "Event.h"
 
