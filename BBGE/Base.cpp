eaf20df478c53717d163250445e1ca794ef8f86f
Valentin Ochs <a@0au.de>
2017-01-13 10:08:25 +0100

    Fix charIsUpper(). Solves issue #15.

--- BBGE/Base.cpp
+++ BBGE/Base.cpp
@@ -218,7 +218,7 @@ static TransatableStaticInit _transtable_static_init;
 
 static unsigned char charIsUpper(unsigned char c)
 {
-	return c == upperToLowerTable[c];
+	return c == lowerToUpperTable[c];
 }
 
 static unsigned char charToLower(unsigned char c)
