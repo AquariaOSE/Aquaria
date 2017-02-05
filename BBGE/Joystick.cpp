f39725634d2af6c4225cda06c6d7806a5bd22e1c
fgenesis <false.genesis@googlemail.com>
2017-02-05 21:25:06 +0100

    Some preparations to compile with SDL1.2 (not yet!)

--- BBGE/Joystick.cpp
+++ BBGE/Joystick.cpp
@@ -19,10 +19,6 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
-#ifndef BBGE_BUILD_SDL2
-#error test this
-#endif
-
 #include "Joystick.h"
 #include "Core.h"
 
