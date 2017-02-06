3b322f2179d01bf0216bf6b1e09e277ef04e6ec0
fgenesis <false.genesis@googlemail.com>
2017-02-06 03:34:41 +0100

    Fix build on osx

--- BBGE/Event.h
+++ BBGE/Event.h
@@ -18,8 +18,8 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
-#ifndef EVENT_H
-#define EVENT_H
+#ifndef BBGE_EVENT_H
+#define BBGE_EVENT_H
 
 #include <stdlib.h>
 
