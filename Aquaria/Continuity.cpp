4d1fa755454163bd6b18c7380753160bdbc4463e
James Le Cuirot <chewi@aura-online.co.uk>
2017-02-09 20:22:59 +0000

    Put tinyxml2 in subdir so that system header is respected

--- Aquaria/Continuity.cpp
+++ Aquaria/Continuity.cpp
@@ -28,7 +28,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "ReadXML.h"
 #include "Web.h"
 
-#include "tinyxml2.h"
+#include <tinyxml2.h>
 using namespace tinyxml2;
 
 #define MAX_EATS			8
