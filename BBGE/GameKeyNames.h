6e3feb8c1a266f3e998ef2146c8386b397e0ee63
fgenesis <false.genesis@googlemail.com>
2017-02-18 22:09:43 +0100

    Turns out using IDs was a bad idea, reverting to KEY_* strings

---
+++ BBGE/GameKeyNames.h
@@ -0,0 +1,9 @@
+#ifndef BBGE_LEGACYKEYCODES_H
+#define BBGE_LEGACYKEYCODES_H
+
+#include <string>
+
+int getInputCodeFromKeyName(const char *name);
+const std::string& getKeyNameFromInputCode(int k);
+
+#endif
