AquariaOSE/Aquaria

commit a7c2d054a2ed6331d159a0b738e3971a1265171e

Begin joystick code refactor; some related cleanups and unused code removal

Aquaria/AquariaMenuItem.cpp
@@ -350,10 +350,6 @@ bool AquariaSlider::doSliderInput(float dt)
 		inputAmount = -0.1f;
 	else if (core->joystick.position.x >= SLIDER_JOY_THRESHOLD)
 		inputAmount = +0.1f;
-	else if (core->joystick.dpadLeft)
-		inputAmount = -0.1f;
-	else if (core->joystick.dpadRight)
-		inputAmount = +0.1f;
 	else if (obj && obj->isActing(ACTION_MENULEFT))
 		inputAmount = -0.1f;
 	else if (obj && obj->isActing(ACTION_MENURIGHT))
@@ -675,7 +671,7 @@ void AquariaKeyConfig::onUpdate(float dt)
 			}
 			else
 			{
-				for (int i = ActionMapper::JOY1_BUTTON_0; i <= ActionMapper::JOY1_BUTTON_16; i++)
+				for (int i = ActionMapper::JOY1_BUTTON_0; i <= MAX_JOYSTICK_BTN; i++)
 				{
 					if (dsq->game->getKeyState(i))
 					{

Aquaria/DSQ.cpp
@@ -1603,12 +1603,6 @@ void DSQ::toggleInputGrabPlat(bool on)
 {
 }
 
-void DSQ::instantQuit()
-{
-	if (core->getCtrlState() && core->getAltState())
-		Core::instantQuit();
-}
-
 int DSQ::getEntityLayerToLayer(int lcode)
 {
 	if (lcode == -4)
@@ -3970,21 +3964,6 @@ void DSQ::onUpdate(float dt)
 
 		if (armb && ActionMapper::getKeyState(armb->joy[0]))
 			mouse.buttons.right = DOWN;
-
-
-
-		// not going to happen anymore!
-		// bye, bye xbox360 controller
-		if (!mouse.buttons.middle)
-		{
-			if (joystick.rightThumb)
-				mouse.buttons.middle = DOWN;
-			else if (joystick.leftThumb)
-				mouse.buttons.middle = DOWN;
-		}
-
-
-
 	}
 
 	if (joystickEnabled)

Aquaria/Game.cpp
@@ -6981,28 +6981,6 @@ void Game::bindInput()
 		addAction(ACTION_TOGGLEGRID, KEY_F9);
 	}
 
-	/*
-	addAction(ACTION_MENULEFT,	KEY_LEFT);
-	addAction(ACTION_MENURIGHT,	KEY_RIGHT);
-	addAction(ACTION_MENUUP,	KEY_UP);
-	addAction(ACTION_MENUDOWN,	KEY_DOWN);
-
-	dsq->user.control.actionSet.importAction(this, "SwimLeft",		ACTION_MENULEFT);
-	dsq->user.control.actionSet.importAction(this, "SwimRight",		ACTION_MENURIGHT);
-	dsq->user.control.actionSet.importAction(this, "SwimUp",		ACTION_MENUUP);
-	dsq->user.control.actionSet.importAction(this, "SwimDown",		ACTION_MENUDOWN);
-
-	addAction(ACTION_MENULEFT,	JOY1_DPAD_LEFT);
-	addAction(ACTION_MENURIGHT,	JOY1_DPAD_RIGHT);
-	addAction(ACTION_MENUUP,	JOY1_DPAD_UP);
-	addAction(ACTION_MENUDOWN,	JOY1_DPAD_DOWN);
-	*/
-
-	addAction(ACTION_MENULEFT,	JOY1_STICK_LEFT);
-	addAction(ACTION_MENURIGHT,	JOY1_STICK_RIGHT);
-	addAction(ACTION_MENUUP,	JOY1_STICK_UP);
-	addAction(ACTION_MENUDOWN,	JOY1_STICK_DOWN);
-
 	// To capture quick song keys via script
 	dsq->user.control.actionSet.importAction(this, "SongSlot1",		ACTION_SONGSLOT1);
 	dsq->user.control.actionSet.importAction(this, "SongSlot2",		ACTION_SONGSLOT2);

BBGE/ActionMapper.h
@@ -27,6 +27,7 @@ class Event;
 class ActionMapper;
 
 #include "ActionSet.h"
+#include "Joystick.h"
 
 typedef std::vector<int> ButtonList;
 
@@ -81,45 +82,11 @@ public:
 		MOUSE_BUTTON_MIDDLE	= 1001,
 
 		JOY1_BUTTON_0		= 2000,
-		JOY1_BUTTON_1		= 2001,
-		JOY1_BUTTON_2		= 2002,
-		JOY1_BUTTON_3		= 2003,
-		JOY1_BUTTON_4		= 2004,
-		JOY1_BUTTON_5		= 2005,
-		JOY1_BUTTON_6		= 2006,
-		JOY1_BUTTON_7		= 2007,
-		JOY1_BUTTON_8		= 2008,
-		JOY1_BUTTON_9		= 2009,
-		JOY1_BUTTON_10		= 2010,
-		JOY1_BUTTON_11		= 2011,
-		JOY1_BUTTON_12		= 2012,
-		JOY1_BUTTON_13		= 2013,
-		JOY1_BUTTON_14		= 2014,
-		JOY1_BUTTON_15		= 2015,
-		JOY1_BUTTON_16		= 2016,
-
-
-		X360_BTN_START		= 3016,
-		X360_BTN_BACK		= 3017,
-
-		JOY1_DPAD_LEFT		= 4000,
-		JOY1_DPAD_RIGHT		= 4001,
-		JOY1_DPAD_DOWN		= 4002,
-		JOY1_DPAD_UP		= 4003,
-		JOY1_STICK_LEFT		= 4010,
-		JOY1_STICK_RIGHT	= 4011,
-		JOY1_STICK_DOWN		= 4012,
-		JOY1_STICK_UP		= 4013,
+		JOY1_BUTTON_END = JOY1_BUTTON_0 + MAX_JOYSTICK_BTN, // one past end
 	};
-
-	enum { DPAD_LEFT = 0, DPAD_RIGHT, DPAD_UP, DPAD_DOWN };
-
-
 
 	virtual void enableInput();
 	virtual void disableInput();
-
-
 
 	Event *addCreatedEvent(Event *event);
 	void clearCreatedEvents();

Aquaria/Avatar.cpp
@@ -5170,38 +5170,6 @@ bool Avatar::canQuickSong()
 	return !isSinging() && !isEntityDead() && isInputEnabled() && quickSongCastDelay <= 0;
 }
 
-void Avatar::updateJoystick(float dt)
-{
-	if (canQuickSong())
-	{
-
-		if (core->joystick.dpadUp)
-		{
-			if (dsq->continuity.hasSong(SONG_ENERGYFORM) && dsq->continuity.form != FORM_ENERGY)
-			{
-				quickSongCastDelay = QUICK_SONG_CAST_DELAY;
-				dsq->continuity.castSong(SONG_ENERGYFORM);
-			}
-		}
-		else if (core->joystick.dpadDown && dsq->continuity.hasSong(SONG_BEASTFORM) && dsq->continuity.form != FORM_BEAST)
-		{
-			quickSongCastDelay = QUICK_SONG_CAST_DELAY;
-			dsq->continuity.castSong(SONG_BEASTFORM);
-		}
-		else if (core->joystick.dpadLeft && dsq->continuity.hasSong(SONG_SUNFORM) && dsq->continuity.form != FORM_SUN)
-		{
-			quickSongCastDelay = QUICK_SONG_CAST_DELAY;
-			dsq->continuity.castSong(SONG_SUNFORM);
-		}
-		else if (core->joystick.dpadRight && dsq->continuity.hasSong(SONG_NATUREFORM) && dsq->continuity.form != FORM_NATURE)
-		{
-			quickSongCastDelay = QUICK_SONG_CAST_DELAY;
-			dsq->continuity.castSong(SONG_NATUREFORM);
-		}
-
-	}
-}
-
 void Avatar::applyRidingPosition()
 {
 	if (riding)
@@ -5725,7 +5693,6 @@ void Avatar::onUpdate(float dt)
 		if (songInterfaceTimer < 1)
 			songInterfaceTimer += dt;
 	}
-	updateJoystick(dt);
 
 	if (quickSongCastDelay>0)
 	{

Aquaria/Avatar.h
@@ -180,7 +180,6 @@ public:
 	Vector getKeyDir();
 
 	void startBurstCommon();
-	void updateJoystick(float dt);
 
 	void openSingingInterface();
 	void closeSingingInterface();

Aquaria/DSQ.h
@@ -1486,8 +1486,6 @@ public:
 	void loadFonts();
 
 
-	void instantQuit();
-
 	void centerText(const std::string &text);
 	void centerMessage(const std::string &text, float y=300, int type=0);
 

Aquaria/UserSettings.cpp
@@ -20,14 +20,9 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 #include "UserSettings.h"
 
-#ifndef AQUARIA_USERSETTINGS_DATAONLY
-	#include "DSQ.h"
-	#include "Game.h"
-	#include "Avatar.h"
-#else
-	#include "tinyxml2.h"
-	using namespace tinyxml2;
-#endif
+#include "DSQ.h"
+#include "Game.h"
+#include "Avatar.h"
 
 
 void UserSettings::save()
@@ -554,7 +549,6 @@ void UserSettings::load(bool doApply, const std::string &overrideFile)
 
 void UserSettings::apply()
 {
-#ifndef AQUARIA_USERSETTINGS_DATAONLY
 	core->sound->setMusicVolume(audio.musvol);
 	core->sound->setSfxVolume(audio.sfxvol);
 	core->sound->setVoiceVolume(audio.voxvol);
@@ -563,6 +557,7 @@ void UserSettings::apply()
 
 	dsq->loops.updateVolume();
 
+	// FIXME: This should be per-joystick
 	core->joystick.s1ax = control.s1ax;
 	core->joystick.s1ay = control.s1ay;
 	core->joystick.s2ax = control.s2ax;
@@ -581,7 +576,5 @@ void UserSettings::apply()
 	dsq->bindInput();
 
 	core->settings.prebufferSounds = audio.prebuffer;
-
-#endif
 }
 

commit 4990ae3bf674b3b1d05b4d5b00f175582dc50763

Fix ACTION_MENU* input to be sent as intended, broke in a7c2d054a2ed6 iirc

Aquaria/AquariaMenuItem.cpp
@@ -163,49 +163,15 @@ Direction AquariaGuiElement::GetDirection()
 {
 	Direction dir = DIR_NONE;
 	
-	// This joystick code is already supposed to send ACTION_MENU*.
-	// Actually some places depend on the actions to be sent,
-	// So checking this here might work for a few cases,
-	// but others will break.
-	// I'll leave this in here for now -- fg
-	/*Vector p;
-	for(size_t i = 0; i < core->getNumJoysticks(); ++i)
-		if(Joystick *j = core->getJoystick(i))
-			if(j->isEnabled())
-			{
-				p = core->getJoystick(i)->position;
-				if(!p.isLength2DIn(0.4f))
-					break;
-			}
+	StateObject *obj = dsq->getTopStateObject(); // usually Game...
+	if (obj)
+	{
+		if (obj->isActing(ACTION_MENULEFT, -1))			dir = DIR_LEFT;
+		else if (obj->isActing(ACTION_MENURIGHT, -1))	dir = DIR_RIGHT;
+		else if (obj->isActing(ACTION_MENUUP, -1))		dir = DIR_UP;
+		else if (obj->isActing(ACTION_MENUDOWN, -1))	dir = DIR_DOWN;
+	}
 
-	if (!p.isLength2DIn(0.4f))
-	{
-		if (fabsf(p.x) > fabsf(p.y))
-		{
-			if (p.x > 0)
-				dir = DIR_RIGHT;
-			else
-				dir = DIR_LEFT;
-		}
-		else
-		{
-			if (p.y > 0)
-				dir = DIR_DOWN;
-			else
-				dir = DIR_UP;
-		}
-	}
-	else*/
-	{
-		StateObject *obj = dsq->getTopStateObject(); // usually Game...
-		if (obj)
-		{
-			if (obj->isActing(ACTION_MENULEFT, -1))			dir = DIR_LEFT;
-			else if (obj->isActing(ACTION_MENURIGHT, -1))	dir = DIR_RIGHT;
-			else if (obj->isActing(ACTION_MENUUP, -1))		dir = DIR_UP;
-			else if (obj->isActing(ACTION_MENUDOWN, -1))	dir = DIR_DOWN;
-		}
-	}
 	return dir;
 }
 
@@ -380,31 +346,13 @@ bool AquariaSlider::doSliderInput(float dt)
 	if (!(core->mouse.position - this->position).isLength2DIn(5))
 		return false;
 
-	float inputAmount;  // How much to adjust by?
-
-	// disabled the jaxis threshold check;
-	// ACTION_MENU* should be sent automatically when above the threshold -- fg
-	/*Vector jpos;
-	for(size_t i = 0; i < core->getNumJoysticks(); ++i)
-		if(Joystick *j = core->getJoystick(i))
-			if(j->isEnabled())
-			{
-				jpos = core->getJoystick(i)->position;
-				if(fabsf(jpos.x) > SLIDER_JOY_THRESHOLD)
-					break;
-			}*/
+	float inputAmount = 0;  // How much to adjust by?
 
 	StateObject *obj = dsq->getTopStateObject();
-	/*if (jpos.x <= -SLIDER_JOY_THRESHOLD)
-		inputAmount = -0.1f;
-	else if (jpos.x >= SLIDER_JOY_THRESHOLD)
-		inputAmount = +0.1f;
-	else*/ if (obj && obj->isActing(ACTION_MENULEFT, -1))
+	if (obj && obj->isActing(ACTION_MENULEFT, -1))
 		inputAmount = -0.1f;
 	else if (obj && obj->isActing(ACTION_MENURIGHT, -1))
 		inputAmount = +0.1f;
-	else
-		inputAmount = 0;
 
 	if (inputAmount != 0)
 	{

Aquaria/DSQ.cpp
@@ -4500,8 +4500,7 @@ void DSQ::initActionButtons()
 	clearActionButtons();
 
 	std::vector<int> allkeys;
-	// Don't need joysticks keys for this
-	for(int i = 0; i < MOUSE_BUTTON_EXTRA_END; ++i)
+	for(int i = 0; i < INTERNALLY_USED_ACTION_BUTTONS_END; ++i)
 		allkeys.push_back(i);
 
 	// create sentinel

Aquaria/Game.cpp
@@ -2527,7 +2527,7 @@ void Game::action(int id, int state, int source, InputDevice device)
 	if(isIgnoreAction((AquariaActions)id))
 		return;
 
-	// forward
+	// forward menu actions
 	if(id == ACTION_TOGGLEMENU)
 	{
 		themenu->action(id, state, source, device);
@@ -2556,16 +2556,6 @@ void Game::action(int id, int state, int source, InputDevice device)
 	{
 		if (id == ACTION_TOGGLEGRID && !state)			toggleGridRender();
 	}
-
-	// Forward these to Lua scripts (because digital swim movement should be seen as menu movement as well)
-	if(id == ACTION_SWIMLEFT)
-		action(ACTION_MENULEFT, state, source, device);
-	else if(id == ACTION_SWIMRIGHT)
-		action(ACTION_MENURIGHT, state, source, device);
-	else if(id == ACTION_SWIMUP)
-		action(ACTION_MENUUP, state, source, device);
-	else if(id == ACTION_SWIMDOWN)
-		action(ACTION_MENUDOWN, state, source, device);
 }
 
 void Game::toggleWorldMap()
@@ -3251,11 +3241,6 @@ void Game::bindInput()
 		as.importAction(this, "SwimLeft",		ACTION_SWIMLEFT, sourceID);
 		as.importAction(this, "SwimRight",		ACTION_SWIMRIGHT, sourceID);
 
-		as.importAction(this, "MenuUp",		ACTION_MENUUP, sourceID);
-		as.importAction(this, "MenuDown",		ACTION_MENUDOWN, sourceID);
-		as.importAction(this, "MenuLeft",		ACTION_MENULEFT, sourceID);
-		as.importAction(this, "MenuRight",		ACTION_MENURIGHT, sourceID);
-
 		as.importAction(this, "PrevPage",		ACTION_PREVPAGE, sourceID);
 		as.importAction(this, "NextPage",		ACTION_NEXTPAGE, sourceID);
 		as.importAction(this, "CookFood",		ACTION_COOKFOOD, sourceID);
@@ -3279,6 +3264,18 @@ void Game::bindInput()
 
 		as.importAction(this, "Look",			ACTION_LOOK, sourceID);
 		as.importAction(this, "Roll",			ACTION_ROLL, sourceID);
+
+		// menu movement via ACTION_SWIM* alias
+		as.importAction(this, "SwimUp",		ACTION_MENUUP, sourceID);
+		as.importAction(this, "SwimDown",		ACTION_MENUDOWN, sourceID);
+		as.importAction(this, "SwimLeft",		ACTION_MENULEFT, sourceID);
+		as.importAction(this, "SwimRight",		ACTION_MENURIGHT, sourceID);
+
+		// menu movement via analog stick
+		addAction(ACTION_MENURIGHT, JOY_STICK_RIGHT, sourceID);
+		addAction(ACTION_MENULEFT, JOY_STICK_LEFT, sourceID);
+		addAction(ACTION_MENUDOWN, JOY_STICK_DOWN, sourceID);
+		addAction(ACTION_MENUUP, JOY_STICK_UP, sourceID);
 	}
 
 	if (avatar)

Aquaria/InGameMenu.cpp
@@ -945,11 +945,6 @@ void InGameMenu::bindInput()
 		as.importAction(this, "FoodLeft",		ACTION_FOODLEFT, sourceID);
 		as.importAction(this, "FoodRight",		ACTION_FOODRIGHT, sourceID);
 		as.importAction(this, "FoodDrop",		ACTION_FOODDROP, sourceID);
-
-		as.importAction(this, "MenuUp",		ACTION_MENUUP, sourceID);
-		as.importAction(this, "MenuDown",		ACTION_MENUDOWN, sourceID);
-		as.importAction(this, "MenuLeft",		ACTION_MENULEFT, sourceID);
-		as.importAction(this, "MenuRight",		ACTION_MENURIGHT, sourceID);
 	}
 }
 

BBGE/ActionMapper.h
@@ -85,9 +85,10 @@ public:
 	bool getKeyState(int k, int sourceID);
 
 	ActionData *getActionDataByIDAndSource(int actionID, int source);
+
 protected:
 
-	std::vector<Event*>createdEvents;
+	std::vector<Event*> createdEvents;
 
 	bool inUpdate;
 	bool inputEnabled;

BBGE/ActionSet.cpp
@@ -160,6 +160,8 @@ void ActionSet::importAction(ActionMapper *mapper, const std::string &name, int 
 			return;
 		}
 	}
+
+	debugLog("ActionSet::importAction: No such action: " + name);
 }
 
 void ActionSet::importAction(ActionMapper *mapper, const std::string &name, Event *event, int state) const
@@ -178,6 +180,8 @@ void ActionSet::importAction(ActionMapper *mapper, const std::string &name, Even
 			return;
 		}
 	}
+
+	debugLog("ActionSet::importAction: No such action: " + name);
 }
 
 ActionInput *ActionSet::addActionInput(const std::string &name)

BBGE/ActionStatus.cpp
@@ -23,6 +23,12 @@ void ActionButtonStatus::import(const ActionSet& as)
 			if(unsigned(inp.data.all[j]) < ACTION_BUTTON_ENUM_SIZE)
 				found[inp.data.all[j]] = 1;
 	}
+
+	// HACK: always query these for menu input emulation
+	found[JOY_STICK_LEFT] = 1;
+	found[JOY_STICK_RIGHT] = 1;
+	found[JOY_STICK_UP] = 1;
+	found[JOY_STICK_DOWN]= 1;
 	
 	toQuery.clear();
 	for(int k = 1; k < sizeof(found); ++k) // ignore [0]
@@ -96,6 +102,15 @@ bool ActionButtonStatus::_queryStatus(int k) const
 
 	if (k >= JOY_BUTTON_0 && k < JOY_BUTTON_END)
 		return j->getButton(k - JOY_BUTTON_0);
+
+	if(k == JOY_STICK_LEFT)
+		return j->position.x < -JOY_AXIS_THRESHOLD;
+	if(k == JOY_STICK_RIGHT)
+		return j->position.x > JOY_AXIS_THRESHOLD;
+	if(k == JOY_STICK_UP)
+		return j->position.y < -JOY_AXIS_THRESHOLD;
+	if(k == JOY_STICK_DOWN)
+		return j->position.y > JOY_AXIS_THRESHOLD;
 
 	if (k >= JOY_AXIS_0_POS && k < JOY_AXIS_END_POS)
 	{

BBGE/ActionStatus.h
@@ -17,7 +17,15 @@ enum ActionButtonType
 	MOUSE_BUTTON_EXTRA_START,
 	MOUSE_BUTTON_EXTRA_END = MOUSE_BUTTON_EXTRA_START + mouseExtraButtons,
 
-	JOY_BUTTON_0		= MOUSE_BUTTON_EXTRA_END,
+	// maps to whatever is configured as the primary joystick x/y axes
+	JOY_STICK_LEFT = MOUSE_BUTTON_EXTRA_END,
+	JOY_STICK_RIGHT,
+	JOY_STICK_UP,
+	JOY_STICK_DOWN,
+
+	INTERNALLY_USED_ACTION_BUTTONS_END, // Engine needs anything above this for handling inputs properly
+
+	JOY_BUTTON_0		= INTERNALLY_USED_ACTION_BUTTONS_END,
 	JOY_BUTTON_END		= JOY_BUTTON_0 + MAX_JOYSTICK_BTN,
 
 	JOY_AXIS_0_POS		= JOY_BUTTON_END,