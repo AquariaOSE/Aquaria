777de27dce16a353cf59d7d8f7145d9038c4c832
fgenesis <false.genesis@googlemail.com>
2022-06-20 04:26:42 +0200

    declutter main. AQconfig.exe is gone, did not produce a proper config anymore  anyway
    plus it was never open sourced, so there's no reason to keep support for it around

--- Aquaria/Main.cpp
+++ Aquaria/Main.cpp
@@ -20,90 +20,37 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
 #include "DSQ.h"
-#include <fstream>
 #include <SDL.h>
 #include <SDL_main.h>
 
-#ifdef BBGE_BUILD_WINDOWS
-	#define WIN32_LEAN_AND_MEAN
-	#include <windows.h>
-	#include <shellapi.h>
-#endif
 
-
-static void MakeRan(void)
+extern "C" int main(int argc,char *argv[])
 {
-#ifdef BBGE_BUILD_WINDOWS
-	std::ofstream out("ran");
-	if(out)
-	{
-		for (int i = 0; i < 32; i++)
-			out << rand()%1000;
-		out.close();
-	}
-#endif
-}
+	std::string dsqParam = ""; // fileSystem
+	std::string extraDataDir = "";
 
-static void StartAQConfig()
-{
-#if defined(BBGE_BUILD_WINDOWS)
-	if (!exists("ran", false, true))
-	{
-		MakeRan();
-		if(exists("AQConfig.exe", false, true))
-		{
-			ShellExecute(NULL, "open", "AQConfig.exe", NULL, NULL, SW_SHOWNORMAL);
-			exit(0);
-		}
-	}
-	remove("ran");
-#endif
-}
-
-static void CheckConfig(void)
-{
-#ifdef BBGE_BUILD_WINDOWS
-	bool hasCfg = exists("usersettings.xml", false, true);
-	if(!hasCfg)
-		StartAQConfig();
-#endif
-}
-
-
-
-	extern "C" int main(int argc,char *argv[])
-	{
-		std::string dsqParam = ""; // fileSystem
-		std::string extraDataDir = "";
-
-		const char *envPath = 0;
+	const char *envPath = 0;
 #ifdef BBGE_BUILD_UNIX
-		envPath = getenv("AQUARIA_DATA_PATH");
-		if (envPath)
-		{
-			dsqParam = envPath;
-		}
+	envPath = getenv("AQUARIA_DATA_PATH");
+	if (envPath)
+	{
+		dsqParam = envPath;
+	}
 #endif
 #ifdef AQUARIA_DEFAULT_DATA_DIR
-		if(!envPath)
-			dsqParam = AQUARIA_DEFAULT_DATA_DIR;
+	if(!envPath)
+		dsqParam = AQUARIA_DEFAULT_DATA_DIR;
 #endif
 #ifdef AQUARIA_EXTRA_DATA_DIR
-		extraDataDir = AQUARIA_EXTRA_DATA_DIR;
+	extraDataDir = AQUARIA_EXTRA_DATA_DIR;
 #endif
 
-
-		CheckConfig();
-
-		{
-			DSQ dsql(dsqParam, extraDataDir);
-			dsql.init();
-			dsql.run();
-			dsql.shutdown();
-		}
-
-		MakeRan();
-
-		return (0);
+	{
+		DSQ dsql(dsqParam, extraDataDir);
+		dsql.init();
+		dsql.run();
+		dsql.shutdown();
 	}
 
+	return (0);
+}
