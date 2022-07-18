AquariaOSE/Aquaria

commit 06270eaac0bc9ccc6165d52ab82dcf149df4e603

remove RenderObject::childGarbage

BBGE/RenderObject.cpp
@@ -137,6 +137,7 @@ RenderObject::RenderObject()
 	idx = -1;
 	_fv = false;
 	_fh = false;
+	_markedForDelete = false;
 	updateCull = -1;
 
 	layer = LR_NONE;
@@ -383,12 +384,6 @@ void RenderObject::destroy()
 	}
 
 	texture = NULL;
-}
-
-const RenderObject &RenderObject::operator=(const RenderObject &r)
-{
-	errorLog("Operator= not defined for RenderObject. Use 'copyProperties'");
-	return *this;
 }
 
 Vector RenderObject::getRealPosition()
@@ -833,18 +828,6 @@ void RenderObject::removeChild(RenderObject *r)
 	}
 }
 
-void RenderObject::enqueueChildDeletion(RenderObject *r)
-{
-	if (r->parent == this)
-	{
-		// Don't garbage a child more than once
-		for (size_t i = 0; i < childGarbage.size(); ++i)
-			if(childGarbage[i] == r)
-				return;
-		childGarbage.push_back(r);
-	}
-}
-
 void RenderObject::safeKill()
 {
 	alpha = 0;
@@ -858,8 +841,7 @@ void RenderObject::safeKill()
 
 	if (this->parent)
 	{
-		parent->enqueueChildDeletion(this);
-
+		_markedForDelete = true;
 	}
 	else
 	{
@@ -920,6 +902,7 @@ void RenderObject::onUpdate(float dt)
 	beforeScaleOffset.update(dt);
 	rotationOffset.update(dt);
 
+	bool hasChildrenToDelete = false;
 	for (Children::iterator i = children.begin(); i != children.end(); i++)
 	{
 		if (shareAlphaWithChildren)
@@ -931,17 +914,27 @@ void RenderObject::onUpdate(float dt)
 		{
 			(*i)->update(dt);
 		}
+		hasChildrenToDelete |= (*i)->_markedForDelete;
+
 	}
 
-	if (!childGarbage.empty())
+	if (hasChildrenToDelete)
 	{
-		for (Children::iterator i = childGarbage.begin(); i != childGarbage.end(); i++)
+		size_t w = 0;
+		const size_t N = children.size();
+		for (size_t i = 0; i < N; ++i)
 		{
-			removeChild(*i);
-			(*i)->destroy();
-			delete (*i);
+			RenderObject *ro = children[i];
+			if(ro->_markedForDelete)
+			{
+				ro->parent = NULL;
+				ro->destroy();
+				delete ro;
+			}
+			else
+				children[w++] = ro;
 		}
-		childGarbage.clear();
+		children.resize(w);
 	}
 
 	if (MotionBlurData *mb = this->motionBlur)

BBGE/RenderObject.h
@@ -240,6 +240,7 @@ public:
 	bool _dead;
 	bool _hidden;
 	bool _fv, _fh;
+	bool _markedForDelete;
 
 	unsigned char pm;  // unsigned char to save space
 
@@ -280,7 +281,7 @@ public:
 	// ----------------------
 
 	typedef std::vector<RenderObject*> Children;
-	Children children, childGarbage;
+	Children children;
 
 protected:
 	RenderObject *parent;
@@ -307,9 +308,6 @@ protected:
 	size_t idx; // index in layer
 	StateData *stateData;
 	MotionBlurData *motionBlur;
-
-private:
-	void enqueueChildDeletion(RenderObject *r);
 };
 
 #endif

commit 9e2c640b518251ead9854a9c680fbbf8fa22e472

attempt to fix crash when loading a saved game on a map with an already collected health upgrade
This problem might have been introduced in 06270eaac0bc9cc but not sure.
The crash could happen due to a use-after-free memory access:
When deleting an entity with skel in postInit(), deletion would be
delayed by 1 frame because that is how entity_delete() works.
During deletion, RenderObject::children were removed and deleted
but SkeletalSprite::bones still had those pointers.
Additionally an object would delete its children in onUpdate() after
safeKill(), and still proceed to run scripts, which would then possibly
access freed memory too.
This fix causes children to not be deleted until we're out of the update()
cycle, and instead delete children together with the root object.

BBGE/RenderObject.cpp
@@ -781,7 +781,8 @@ void RenderObject::onUpdate(float dt)
 {
 	if (isDead()) return;
 
-	updateLife(dt);
+	if(!updateLife(dt))
+		return;
 
 	// FIXME: We might not need to do lifetime checks either; I just
 	// left that above for safety since I'm not certain.  --achurch
@@ -863,7 +864,7 @@ void RenderObject::onUpdate(float dt)
 	}
 }
 
-void RenderObject::updateLife(float dt)
+bool RenderObject::updateLife(float dt)
 {
 	if (decayRate > 0)
 	{
@@ -871,6 +872,7 @@ void RenderObject::updateLife(float dt)
 		if (life<=0)
 		{
 			safeKill();
+			return false;
 		}
 	}
 	if (fadeAlphaWithLife && !alpha.isInterpolating())
@@ -878,6 +880,7 @@ void RenderObject::updateLife(float dt)
 
 		alpha = life/maxLife;
 	}
+	return true;
 }
 
 void RenderObject::unloadDevice()

BBGE/RenderObject.h
@@ -295,7 +295,7 @@ protected:
 	virtual void deathNotify(RenderObject *r);
 	virtual void onEndOfLife() {}
 
-	void updateLife(float dt);
+	bool updateLife(float dt);
 
 	// Is this object or any of its children rendered in pass "pass"?
 	bool hasRenderPass(const int pass) const;

BBGE/SkeletalSprite.cpp
@@ -87,6 +87,10 @@ Bone::Bone() : CollideQuad()
 	stripVert = false;
 }
 
+Bone::~Bone()
+{
+}
+
 ParticleEffect *Bone::getEmitter(unsigned slot) const
 {
 	return slot < emitters.size() ? emitters[slot] : NULL;
@@ -805,6 +809,12 @@ SkeletalSprite::SkeletalSprite() : RenderObject()
 
 SkeletalSprite::~SkeletalSprite()
 {
+}
+
+void SkeletalSprite::destroy()
+{
+	bones.clear(); // they are added as children too, so the next call will do the actual deletion
+	RenderObject::destroy();
 }
 
 void SkeletalSprite::setAnimationKeyNotify(RenderObject *r)

BBGE/SkeletalSprite.h
@@ -47,6 +47,7 @@ class Bone : public CollideQuad
 	friend class SkeletalSprite;
 public:
 	Bone();
+	virtual ~Bone();
 	void setAnimated(int a);
 
 	enum {
@@ -227,6 +228,7 @@ public:
 
 	SkeletalSprite();
 	virtual ~SkeletalSprite();
+	virtual void destroy();
 
 	void loadSkeletal(const std::string &fn);
 	bool saveSkeletal(const std::string &fn);