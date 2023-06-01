AquariaOSE/Aquaria

commit 70b8dcdc3a91c9c6e02af1bc830c99f23b428ac8

Rework texture loading, part 1
Major code/logic cleanups + it has a multithreaded batch mode now.
The VFS code doesn't like multiple threads at all,
so for now there's a big lock in front of where it matters.
Changed precacher, map/tileset, and worldmap loading to batched mode.

Still TODO:
- fix broken mod preview images
- reloading resources on debug key
- make mod recache entirely unnecessary
- actually drop resources when no longer needed

Aquaria/Game.cpp
@@ -231,7 +231,6 @@ Game::Game() : StateObject()
 Game::~Game()
 {
 	delete themenu;
-	tileCache.clean();
 	game = 0;
 }
 
@@ -410,7 +409,7 @@ void Game::fillGridFromQuad(Quad *q, ObsType obsType, bool trim)
 		tpos.y -= h2;
 
 		int w = 0, h = 0;
-		unsigned int size = 0;
+		size_t size = 0;
 		unsigned char *data = q->texture->getBufferAndSize(&w, &h, &size);
 		if (!data)
 		{
@@ -1082,7 +1081,7 @@ void Game::generateCollisionMask(Bone *q, float overrideCollideRadius /* = 0 */)
 		tpos.y -= h2;
 
 		int w = 0, h = 0;
-		unsigned int size = 0;
+		size_t size = 0;
 		unsigned char *data = q->texture->getBufferAndSize(&w, &h, &size);
 		if (!data)
 		{
@@ -1285,7 +1284,6 @@ bool Game::loadSceneXML(std::string scene)
 {
 	bgSfxLoop = "";
 	airSfxLoop = "";
-	elementTemplatePack = "Main";
 	entitySaveData.clear();
 	std::string fn = getSceneFilename(scene);
 	if (!exists(fn))
@@ -1317,17 +1315,13 @@ bool Game::loadSceneXML(std::string scene)
 	if (level)
 	{
 		XMLElement *levelSF = saveFile->NewElement("Level");
-		if (level->Attribute("tileset"))
+		const char *tileset = level->Attribute("tileset");
+		if(!tileset)
+			tileset = level->Attribute("elementTemplatePack"); // legacy, still present in some very old maps
+		if (tileset)
 		{
-			elementTemplatePack = level->Attribute("tileset");
-			loadElementTemplates(elementTemplatePack);
-			levelSF->SetAttribute("tileset", elementTemplatePack.c_str());
-		}
-		else if (level->Attribute("elementTemplatePack"))
-		{
-			elementTemplatePack = level->Attribute("elementTemplatePack");
-			loadElementTemplates(elementTemplatePack);
-			levelSF->SetAttribute("tileset", elementTemplatePack.c_str());
+			loadElementTemplates(tileset);
+			levelSF->SetAttribute("tileset", tileset);
 		}
 		else
 			return false;
@@ -1729,204 +1723,193 @@ bool Game::loadSceneXML(std::string scene)
 		saveFile->InsertEndChild(newSF);
 	}
 
-	std::vector<Element*> loadedElements;
-	loadedElements.reserve(200);
+	struct ElementDef
+	{
+		ElementDef(int lr)
+			: layer(lr), idx(0), x(0), y(0), rot(0), fh(0), fv(0), flags(0), efxIdx(0), repeat(0)
+			, tag(0), sx(1), sy(1), rsx(1), rsy(1)
+		{}
+
+		int layer, idx, x, y, rot, fh, fv, flags, efxIdx, repeat, tag;
+		float sx, sy, rsx, rsy;
+	};
+	std::vector<ElementDef> elemsDefs;
+	elemsDefs.reserve(256);
+
 	XMLElement *simpleElements = doc.FirstChildElement("SE");
 	while (simpleElements)
 	{
-		int idx, x, y, rot;
-		float sz,sz2;
-		loadedElements.clear();
-		if (simpleElements->Attribute("d"))
-		{
-			SimpleIStringStream is(simpleElements->Attribute("d"));
-			while (is >> idx)
-			{
-				is >> x >> y >> rot;
-				Element *e = createElement(idx, Vector(x,y), 4);
-				e->rotation.z = rot;
-				loadedElements.push_back(e);
-			}
-		}
-		if (simpleElements->Attribute("e"))
-		{
-			SimpleIStringStream is2(simpleElements->Attribute("e"));
-			int l = atoi(simpleElements->Attribute("l"));
-			while(is2 >> idx)
-			{
-				is2 >> x >> y >> rot;
-				Element *e = createElement(idx, Vector(x,y), l);
-				e->rotation.z = rot;
-				loadedElements.push_back(e);
-			}
-		}
-		if (simpleElements->Attribute("f"))
-		{
-			SimpleIStringStream is2(simpleElements->Attribute("f"));
-			int l = atoi(simpleElements->Attribute("l"));
-			while(is2 >> idx)
-			{
-				is2 >> x >> y >> rot >> sz;
-				Element *e = createElement(idx, Vector(x,y), l);
-				e->scale = Vector(sz,sz);
-				e->rotation.z = rot;
-				loadedElements.push_back(e);
-			}
-		}
-		if (simpleElements->Attribute("g"))
-		{
-			SimpleIStringStream is2(simpleElements->Attribute("g"));
-			int l = atoi(simpleElements->Attribute("l"));
-			while(is2 >> idx)
-			{
-				int fh, fv;
-				is2 >> x >> y >> rot >> sz >> fh >> fv;
-				Element *e = createElement(idx, Vector(x,y), l);
-				if (fh)
-					e->flipHorizontal();
-				if (fv)
-					e->flipVertical();
-				e->scale = Vector(sz,sz);
-				e->rotation.z = rot;
-				loadedElements.push_back(e);
-			}
-		}
-		if (simpleElements->Attribute("h"))
-		{
-			SimpleIStringStream is2(simpleElements->Attribute("h"));
-			int l = atoi(simpleElements->Attribute("l"));
-			while(is2 >> idx)
-			{
-				int fh, fv;
-				int flags;
-				is2 >> x >> y >> rot >> sz >> fh >> fv >> flags;
-				Element *e = createElement(idx, Vector(x,y), l);
-				e->elementFlag = (ElementFlag)flags;
-				if (e->elementFlag >= EF_MAX || e->elementFlag < EF_NONE)
-					e->elementFlag = EF_NONE;
-				if (fh)
-					e->flipHorizontal();
-				if (fv)
-					e->flipVertical();
-				e->scale = Vector(sz,sz);
-				e->rotation.z = rot;
-				loadedElements.push_back(e);
-			}
-		}
-		if (simpleElements->Attribute("i"))
-		{
-			SimpleIStringStream is2(simpleElements->Attribute("i"));
-			int l = atoi(simpleElements->Attribute("l"));
-			while(is2 >> idx)
-			{
-				int fh, fv;
-				int flags;
-				int efxIdx;
-				is2 >> x >> y >> rot >> sz >> fh >> fv >> flags >> efxIdx;
-				if (sz < MIN_SIZE)
-					sz = MIN_SIZE;
-				Element *e = createElement(idx, Vector(x,y), l);
-				e->elementFlag = (ElementFlag)flags;
-				if (fh)
-					e->flipHorizontal();
-				if (fv)
-					e->flipVertical();
+		const size_t defsBeginIdx = elemsDefs.size();
+		const int layer = atoi(simpleElements->Attribute("l"));
 
-				e->scale = Vector(sz,sz);
-				e->rotation.z = rot;
-				e->setElementEffectByIndex(efxIdx);
-				loadedElements.push_back(e);
+		if (const char *attr = simpleElements->Attribute("d"))
+		{
+			SimpleIStringStream is(attr, SimpleIStringStream::REUSE);
+			ElementDef d(4); // legacy crap
+			while (is >> d.idx)
+			{
+				is >> d.x >> d.y >> d.rot;
+				elemsDefs.push_back(d);
 			}
 		}
-		if (simpleElements->Attribute("j"))
+		if (const char *attr = simpleElements->Attribute("e"))
 		{
-			SimpleIStringStream is2(simpleElements->Attribute("j"));
-			int l = atoi(simpleElements->Attribute("l"));
-			while(is2 >> idx)
+			SimpleIStringStream is(attr, SimpleIStringStream::REUSE);
+			ElementDef d(layer);
+			while(is >> d.idx)
 			{
-				int fh, fv;
-				int flags;
-				int efxIdx;
-				int repeat;
-				is2 >> x >> y >> rot >> sz >> fh >> fv >> flags >> efxIdx >> repeat;
-				if (sz < MIN_SIZE)
-					sz = MIN_SIZE;
-				Element *e = createElement(idx, Vector(x,y), l);
-				e->elementFlag = (ElementFlag)flags;
-				if (fh)
-					e->flipHorizontal();
-				if (fv)
-					e->flipVertical();
-
-				e->scale = Vector(sz,sz);
-				e->rotation.z = rot;
-				e->setElementEffectByIndex(efxIdx);
-				if (repeat)
-					e->repeatTextureToFill(true);
-				loadedElements.push_back(e);
+				is >> d.x >> d.y >> d.rot;
+				elemsDefs.push_back(d);
 			}
 		}
-		if (simpleElements->Attribute("k"))
+		if (const char *attr = simpleElements->Attribute("f"))
 		{
-			SimpleIStringStream is2(simpleElements->Attribute("k"));
-			int l = atoi(simpleElements->Attribute("l"));
-			while(is2 >> idx)
+			SimpleIStringStream is(attr, SimpleIStringStream::REUSE);
+			ElementDef d(layer);
+			while(is >> d.idx)
 			{
-				int fh, fv;
-				int flags;
-				int efxIdx;
-				int repeat;
-				is2 >> x >> y >> rot >> sz >> sz2 >> fh >> fv >> flags >> efxIdx >> repeat;
-				if (sz < MIN_SIZE)
-					sz = MIN_SIZE;
-				if (sz2 < MIN_SIZE)
-					sz2 = MIN_SIZE;
-				Element *e = createElement(idx, Vector(x,y), l);
-				e->elementFlag = (ElementFlag)flags;
-				if (fh)
-					e->flipHorizontal();
-				if (fv)
-					e->flipVertical();
-
-				e->scale = Vector(sz,sz2);
-				e->rotation.z = rot;
-				e->setElementEffectByIndex(efxIdx);
-				if (repeat)
-					e->repeatTextureToFill(true);
-
-				loadedElements.push_back(e);
+				is >> d.x >> d.y >> d.rot >> d.sx;
+				d.sy = d.sx;
+				elemsDefs.push_back(d);
 			}
 		}
-		if (simpleElements->Attribute("repeatScale"))
+		if (const char *attr = simpleElements->Attribute("g"))
 		{
-			SimpleIStringStream is2(simpleElements->Attribute("repeatScale"), SimpleIStringStream::REUSE);
-			for(size_t i = 0; i < loadedElements.size(); ++i)
+			SimpleIStringStream is(attr, SimpleIStringStream::REUSE);
+			ElementDef d(layer);
+			while(is >> d.idx)
 			{
-				Element *e = loadedElements[i];
-				if(e->isRepeatingTextureToFill())
+				is >> d.x >> d.y >> d.rot >> d.sx >> d.fh >> d.fv;
+				d.sy = d.sx;
+				elemsDefs.push_back(d);
+			}
+		}
+		if (const char *attr = simpleElements->Attribute("h"))
+		{
+			SimpleIStringStream is(attr, SimpleIStringStream::REUSE);
+			ElementDef d(layer);
+			while(is >> d.idx)
+			{
+				is >> d.x >> d.y >> d.rot >> d.sx >> d.fh >> d.fv >> d.flags;
+				d.sy = d.sx;
+				elemsDefs.push_back(d);
+			}
+		}
+		if (const char *attr = simpleElements->Attribute("i"))
+		{
+
+			SimpleIStringStream is(attr, SimpleIStringStream::REUSE);
+			ElementDef d(layer);
+			while(is >> d.idx)
+			{
+				is >> d.x >> d.y >> d.rot >> d.sx >> d.fh >> d.fv >> d.flags >> d.efxIdx;
+				d.sy = d.sx;
+				elemsDefs.push_back(d);
+			}
+		}
+		if (const char *attr = simpleElements->Attribute("j"))
+		{
+			SimpleIStringStream is(attr, SimpleIStringStream::REUSE);
+			ElementDef d(layer);
+			while(is >> d.idx)
+			{
+				is >> d.x >> d.y >> d.rot >> d.sx >> d.fh >> d.fv >> d.flags >> d.efxIdx >> d.repeat;
+				d.sy = d.sx;
+				elemsDefs.push_back(d);
+			}
+		}
+		if (const char *attr = simpleElements->Attribute("k"))
+		{
+			SimpleIStringStream is(attr, SimpleIStringStream::REUSE);
+			ElementDef d(layer);
+			while(is >> d.idx)
+			{
+				is >> d.x >> d.y >> d.rot >> d.sx >> d.sy >> d.fh >> d.fv >> d.flags >> d.efxIdx >> d.repeat;
+				elemsDefs.push_back(d);
+			}
+		}
+
+		// done loading raw data, now for some possible extensions added later
+
+		if (const char *attr = simpleElements->Attribute("repeatScale"))
+		{
+			SimpleIStringStream is(attr, SimpleIStringStream::REUSE);
+			for(size_t i = defsBeginIdx; i < elemsDefs.size(); ++i)
+			{
+				ElementDef& d = elemsDefs[i];
+				if(d.repeat)
 				{
-					float repeatScaleX = 1, repeatScaleY = 1;
-					if(!(is2 >> repeatScaleX >> repeatScaleY))
+					if(!(is >> d.rsx >> d.rsx))
 						break;
-					e->repeatToFillScale.x = repeatScaleX;
-					e->repeatToFillScale.y = repeatScaleY;
-					e->refreshRepeatTextureToFill();
 				}
 			}
 		}
-		if (simpleElements->Attribute("tag"))
+		if (const char *attr = simpleElements->Attribute("tag"))
 		{
-			SimpleIStringStream is2(simpleElements->Attribute("tag"), SimpleIStringStream::REUSE);
-			for(size_t i = 0; i < loadedElements.size(); ++i)
+			SimpleIStringStream is(attr, SimpleIStringStream::REUSE);
+			for(size_t i = defsBeginIdx; i < elemsDefs.size(); ++i)
 			{
-				Element *e = loadedElements[i];
-				int tag = 0;
-				if(!(is2 >> tag))
+				ElementDef& d = elemsDefs[i];
+				if(!(is >> d.tag))
 					break;
-				e->setTag(tag);
 			}
 		}
 		simpleElements = simpleElements->NextSiblingElement("SE");
+	}
+
+	// figure out which textures in the tileset are used and preload those that are actually used
+	{
+		unsigned char usedIdx[1024] = {0};
+		for(size_t i = 0; i < elemsDefs.size(); ++i)
+		{
+			unsigned idx = elemsDefs[i].idx;
+			if(idx < Countof(usedIdx))
+				usedIdx[idx] = 1;
+		}
+		std::vector<std::string> usedTex;
+		usedTex.reserve(elementTemplates.size()); // optimistically assume all textures in the tileset are used
+
+		for (size_t i = 0; i < elementTemplates.size(); i++)
+		{
+			unsigned idx = elementTemplates[i].idx;
+			if (idx < Countof(usedIdx) && usedIdx[idx])
+				usedTex.push_back(elementTemplates[i].gfx);
+		}
+		std::sort(usedTex.begin(), usedTex.end());
+		// drop duplicates
+		usedTex.resize(std::distance(usedTex.begin(), std::unique(usedTex.begin(), usedTex.end())));
+
+		std::ostringstream os;
+		os << "Scene has " << elemsDefs.size() << " elements that use " << usedTex.size()
+			<< " distinct textures out of the " << elementTemplates.size() << " entries in the tileset";
+		debugLog(os.str());
+
+		// preload all used textures
+		if(usedTex.size())
+			dsq->texmgr.loadBatch(NULL, &usedTex[0], usedTex.size());
+	}
+
+	// Now that all SE tags have been processed, spawn them
+	for(size_t i = 0; i < elemsDefs.size(); ++i)
+	{
+		const ElementDef& d = elemsDefs[i];
+
+		Element *e = createElement(d.idx, Vector(d.x,d.y), d.layer);
+		e->elementFlag = (ElementFlag)d.flags;
+		if (d.fh)
+			e->flipHorizontal();
+		if (d.fv)
+			e->flipVertical();
+
+		e->scale = Vector(d.sx, d.sy);
+		e->rotation.z = d.rot;
+		e->repeatToFillScale.x = d.rsx;
+		e->repeatToFillScale.y = d.rsy;
+		if(d.efxIdx)
+			e->setElementEffectByIndex(d.efxIdx);
+		if (d.repeat)
+			e->repeatTextureToFill(true); // also applies repeatToFillScale
+		e->setTag(d.tag);
 	}
 
 	XMLElement *element = doc.FirstChildElement("Element");
@@ -2783,7 +2766,6 @@ void Game::applyState()
 	//sceneColor = Vector(0.75, 0.75, 0.8);
 	sceneColor = Vector(1,1,1);
 	sceneName = "";
-	elementTemplatePack ="";
 	clearGrid();
 	bg = 0;
 	bg2 = 0;
@@ -2950,9 +2932,9 @@ void Game::applyState()
 	bindInput();
 
 	if (verbose) debugLog("Loading Scene");
-	if (!loadScene(sceneToLoad))
+	loadScene(sceneToLoad);
 	{
-		loadElementTemplates(elementTemplatePack);
+		debugLog("Failed to load scene [" + sceneToLoad + "]");
 	}
 	if (verbose) debugLog("...Done");
 
@@ -4934,31 +4916,16 @@ void Game::loadElementTemplates(std::string pack)
 
 	elementTemplates.clear();
 
-	// HACK: need to uncache things! causes memory leak currently
-	bool doPrecache=false;
 	std::string fn;
-
 	if (dsq->mod.isActive())
 		fn = dsq->mod.getPath() + "tilesets/" + pack + ".txt";
 	else
 		fn = "data/tilesets/" + pack + ".txt";
 
-
-	if (lastTileset == fn)
-	{
-		doPrecache=false;
-	}
-
-	lastTileset = fn;
 	if (!exists(fn))
 	{
-		errorLog ("Could not open element template pack [" + fn + "]");
+		errorLog ("Could not open tileset [" + fn + "]");
 		return;
-	}
-
-	if (doPrecache)
-	{
-		tileCache.clean();
 	}
 
 	InStream in(fn.c_str());
@@ -4977,21 +4944,12 @@ void Game::loadElementTemplates(std::string pack)
 		t.w = w;
 		t.h = h;
 		elementTemplates.push_back(t);
-		if (doPrecache)
-			tileCache.precacheTex(gfx);
 	}
 	in.close();
 
-	for (size_t i = 0; i < elementTemplates.size(); i++)
-	{
-		for (size_t j = i; j < elementTemplates.size(); j++)
-		{
-			if (elementTemplates[i].idx > elementTemplates[j].idx)
-			{
-				std::swap(elementTemplates[i], elementTemplates[j]);
-			}
-		}
-	}
+
+	std::sort(elementTemplates.begin(), elementTemplates.end());
+
 	for (int i = 0; i < 27; i++)
 	{
 		elementTemplates.push_back(getElementTemplateForLetter(i));

Aquaria/Game.h
@@ -88,6 +88,7 @@ class ElementTemplate
 {
 public:
 	ElementTemplate() { alpha = 1; cull = true; w=-1; h=-1; idx=-1; tu1=tu2=tv1=tv2=0; }
+	inline bool operator<(const ElementTemplate& o) const { return idx < o.idx; }
 	std::string gfx;
 	std::vector <TileVector> grid;
 	int w,h;
@@ -293,8 +294,6 @@ public:
 	std::vector<EntitySaveData> entitySaveData;
 	int getIdxForEntityType(std::string type);
 
-	Precacher tileCache;
-
 	void setCameraFollow(Vector *position);
 	Shot *fireShot(const std::string &bankShot, Entity *firer, Entity *target=0, const Vector &pos=Vector(0,0,0), const Vector &aim=Vector(0,0,0), bool playSfx=true);
 	void playBurstSound(bool wallJump=false);
@@ -474,9 +473,6 @@ protected:
 	bool controlHint_ignoreClear;
 	BitmapText *controlHint_text;
 
-	std::string lastTileset;
-
-
 	void createLi();
 	void createPets();
 	Quad *backdropQuad;
@@ -497,9 +493,6 @@ protected:
 
 	Vector getClosestPointOnTriangle(Vector a, Vector b, Vector c, Vector p);
 	Vector getClosestPointOnLine(Vector a, Vector b, Vector p);
-
-	std::string elementTemplatePack;
-
 
 	Vector *cameraFollow;
 	RenderObject *cameraFollowObject;

BBGE/TextureMgr.cpp
@@ -1,0 +1,318 @@
+#include "TextureMgr.h"
+#include <sstream>
+#include <SDL.h>
+#include "MT.h"
+#include "Image.h"
+#include "Base.h"
+#include "Localization.h"
+
+struct TexLoadTmp
+{
+    TexLoadTmp() : curTex(NULL), success(false) { img.pixels = NULL; }
+    std::string name, filename;
+    ImageData img;
+    TextureMgr::LoadMode loadmode;
+    Texture *curTex; // immutable
+    bool success; // if this is true and img.pixels is NULL, don't change anything
+    //bool mipmap;
+};
+
+typedef ImageData (*ImageLoadFunc)(const char *fn);
+
+struct TexLoader
+{
+    TexLoader(ImageLoadFunc f, const std::string& s, const std::string& name = std::string())
+        : loader(f), fn(s), name(name) {}
+    ImageLoadFunc loader;
+    std::string fn;
+    std::string name; // usually empty, but may specify tex name in case cleanups had to be done. always lowercase.
+    ImageData load() const { return loader(fn.c_str()); }
+};
+
+static ImageData loadGeneric(const char *fn)
+{
+    return imageLoadGeneric(fn, false);
+}
+
+static const std::string fixup(const std::string& fn)
+{
+    std::string file = localisePathInternalModpath(fn);
+	file = adjustFilenameCase(file);
+    return file;
+}
+
+
+struct ExtAndLoader
+{
+    const char *ext;
+    ImageLoadFunc func;
+};
+
+static const ExtAndLoader s_extAndLoader[] =
+{
+    { "png", loadGeneric },
+    { "jpg", loadGeneric },
+    { "zga", imageLoadZGA },
+    { "tga", loadGeneric },
+    { NULL, NULL }
+};
+
+static TexLoader getFullnameAndLoader(const std::string& name, const std::string& basedir)
+{
+    // TODO: use localisePath()
+    // TODO: assert that we don't start with . or /
+
+    if(exists(name))
+    {
+        // check first if name exists and has a known extension, if so, use correct loader
+        size_t lastdot = name.rfind('.');
+        size_t lastslash = name.rfind('/');
+        if(lastdot != std::string::npos && (lastslash == std::string::npos || lastslash < lastdot))
+        {
+            std::string ext = name.substr(lastdot + 1);
+            for(const ExtAndLoader *exl = &s_extAndLoader[0]; exl->func; ++exl)
+            {
+                if(ext == exl->ext)
+                {
+                    // remove basedir and extension
+                    std::string texname = name.substr(basedir.length(), name.length() - (basedir.length() + ext.length() + 1)); // strip extension
+                    return TexLoader(exl->func, fixup(name), texname );
+                }
+            }
+        }
+    }
+
+    std::string fn = name;
+    for(const ExtAndLoader *exl = &s_extAndLoader[0]; exl->func; ++exl)
+    {
+        fn.resize(name.length());
+        fn += '.';
+        fn += exl->ext;
+        if(exists(fn))
+            return TexLoader(exl->func, fixup(fn));
+    }
+
+    return TexLoader(NULL, std::string());
+}
+
+void TextureMgr::th_loadFromFile(TexLoadTmp& tt) const
+{
+    std::string withoutExt;
+    for(size_t i = 0; i < loadFromPaths.size(); ++i)
+    {
+        withoutExt = loadFromPaths[i] + tt.name;
+        TexLoader ldr = getFullnameAndLoader(withoutExt, loadFromPaths[i]);
+        if(!ldr.name.empty()) // name was cleaned up, use the updated one
+            tt.name = ldr.name;
+        if(ldr.loader)
+        {
+            if(tt.loadmode < OVERWRITE && tt.curTex && tt.curTex->success && tt.curTex->filename == ldr.fn)
+            {
+                tt.success = true;
+                break;
+            }
+
+            tt.filename = ldr.fn;
+            tt.img = ldr.load();
+            tt.success = !!tt.img.pixels;
+            return;
+        }
+    }
+
+    tt.img.pixels = NULL;
+}
+
+
+TextureMgr::TextureMgr()
+    : sem(SDL_CreateSemaphore(0))
+{
+}
+
+TextureMgr::~TextureMgr()
+{
+    for(size_t i = 0; i < threads.size(); ++i)
+        worktodo.push(NULL); // signal all threads to exit
+    for(size_t i = 0; i < threads.size(); ++i)
+        SDL_WaitThread((SDL_Thread*)threads[i], NULL);
+
+    SDL_DestroySemaphore((SDL_sem*)sem);
+}
+
+size_t TextureMgr::spawnThreads(size_t n)
+{
+    for(size_t i = 0; i < n; ++i)
+    {
+        SDL_Thread *worker;
+#if SDL_VERSION_ATLEAST(2,0,0)
+		worker = SDL_CreateThread(Th_Main, "texldr", this);
+#else
+		worker = SDL_CreateThread(Th_Main, this);
+#endif
+        if(!worker)
+            return i;
+
+        threads.push_back(worker);
+    }
+    return n;
+}
+
+size_t TextureMgr::getNumLoaded() const
+{
+    return cache.size();
+}
+
+Texture* TextureMgr::getOrLoad(const std::string& name)
+{
+    return load(name, KEEP);
+}
+
+void TextureMgr::shutdown()
+{
+    for(TexCache::iterator it = cache.begin(); it != cache.end(); ++it)
+        it->second->unload();
+    cache.clear();
+}
+
+int TextureMgr::Th_Main(void* ud)
+{
+    TextureMgr *self = (TextureMgr*)ud;
+    self->thMain();
+    return 0;
+}
+
+void TextureMgr::thMain()
+{
+    for(;;)
+    {
+        void *p;
+        worktodo.pop(p);
+        if(!p) // a pushed NULL is the signal to exit
+            break;
+
+        TexLoadTmp *tt = (TexLoadTmp*)p;
+        th_loadFromFile(*tt);
+
+        workdone.push(p);
+    }
+}
+
+Texture *TextureMgr::finalize(const TexLoadTmp& tt)
+{
+    Texture *tex = tt.curTex;
+    if(!tex)
+    {
+        tex = new Texture; // always make sure a valid Texture object comes out
+        tex->name = tt.name; // this doesn't ever change
+        cache[tt.name] = tex; // didn't exist, cache now
+    }
+
+    tex->filename = tt.filename;
+    tex->success = tt.success;
+
+    if(!tt.success)
+    {
+        debugLog("FAILED TO LOAD TEXTURE: [" + tt.name + "]");
+        tex->unload();
+        tex->width = 64;
+        tex->height = 64;
+    }
+    if(tt.img.pixels)
+    {
+        //debugLog("LOADED TEXTURE FROM DISK: [" + tt.name + "]");
+        tex->upload(tt.img, /*tt.mipmap*/ true);
+        free(tt.img.pixels);
+    }
+    return tex;
+}
+
+void TextureMgr::loadBatch(Texture * pdst[], const std::string texnames[], size_t n, LoadMode mode, ProgressCallback cb, void *cbUD)
+{
+    if(threads.empty())
+    {
+        for(size_t i = 0; i < n; ++i)
+        {
+            Texture *tex = load(texnames[i], mode);
+            if(pdst)
+                pdst[i] = tex;
+        }
+        return;
+    }
+
+    // Important that this is pre-allocated. We store pointers to elements and
+    // send them to threads, so this must never reallocate.
+    std::vector<TexLoadTmp> tmp(n);
+
+    size_t inprogress = 0, doneCB = 0;
+    for(size_t i = 0; i < n; ++i)
+    {
+        TexLoadTmp& tt = tmp[i];
+        tt.name = texnames[i];
+        stringToLower(tt.name);
+        TexCache::iterator it = cache.find(tt.name);
+        tt.curTex = it != cache.end() ? it->second.content() : NULL;
+        if(mode == KEEP && tt.curTex && tt.curTex->success)
+        {
+            if(pdst)
+                pdst[i] = tt.curTex;
+            if(cb)
+                cb(++doneCB, cbUD);
+            continue;
+        }
+
+        tt.loadmode = mode;
+
+        worktodo.push(&tt);
+        ++inprogress;
+    }
+
+    for(size_t i = 0; i < inprogress; ++i)
+    {
+        void *p;
+        workdone.pop(p);
+        const TexLoadTmp& tt = *(const TexLoadTmp*)p;
+        Texture *tex = finalize(tt);
+        if(pdst)
+            pdst[i] = tex;
+        if(cb)
+            cb(++doneCB, cbUD);
+    }
+}
+
+Texture* TextureMgr::load(const std::string& texname, LoadMode mode)
+{
+    TexLoadTmp tt;
+    tt.name = texname;
+    stringToLower(tt.name);
+    TexCache::iterator it = cache.find(tt.name);
+    tt.curTex = it != cache.end() ? it->second.content() : NULL;
+
+    // texname "" will never load, so don't even try once we have a valid object
+    if(tt.curTex && (texname.empty() || (mode == KEEP && tt.curTex->success)))
+        return tt.curTex;
+
+    tt.loadmode = mode;
+    th_loadFromFile(tt);
+    return finalize(tt);
+}
+
+void TextureMgr::reloadAll(LoadMode mode)
+{
+    std::vector<std::string> todo;
+    for(TexCache::iterator it = cache.begin(); it != cache.end(); ++it)
+    {
+        Texture *tex = it->second.content();
+        if(mode == KEEP && tex->success)
+            continue;
+
+        todo.push_back(tex->name);
+    }
+
+    std::ostringstream os;
+    os << "TextureMgr: Potentially reloading up to " << todo.size() << " textures...";
+    debugLog(os.str());
+
+    if(!todo.empty())
+        loadBatch(NULL, &todo[0],todo.size(), mode);
+}
+
+

Aquaria/AnimationEditor.cpp
@@ -1153,7 +1153,7 @@ void AnimationEditor::clearRot()
 	if (editingBone)
 	{
 		if(core->getCtrlState())
-			editingBone->texture->reload();
+			core->texmgr.load(editingBone->texture->name, TextureMgr::OVERWRITE);
 		else if(splinegrid)
 			splinegrid->resetControlPoints();
 		else

commit 825670f651cc4458f990970760d5611b33e6a017

More fixes for breakage introduced in 70b8dcdc3a91c:
- fix typo that broke repeat-scale loading
- fix broken tile effects loading
- fix randomly shuffled world map tiles (as textures were loaded in a random order)
- reload tileset on editor reload (because we want to edit textures duh)

Aquaria/Game.cpp
@@ -227,6 +227,7 @@ Game::Game() : StateObject()
 	cookingScript = 0;
 	doScreenTrans = false;
 	noSceneTransitionFadeout = false;
+	fullTilesetReload = false;
 }
 
 Game::~Game()
@@ -1725,7 +1726,7 @@ bool Game::loadSceneXML(std::string scene)
 	struct ElementDef
 	{
 		ElementDef(int lr)
-			: layer(lr), idx(0), x(0), y(0), rot(0), fh(0), fv(0), flags(0), efxIdx(0), repeat(0)
+			: layer(lr), idx(0), x(0), y(0), rot(0), fh(0), fv(0), flags(0), efxIdx(-1), repeat(0)
 			, tag(0), sx(1), sy(1), rsx(1), rsy(1)
 		{}
 
@@ -1838,7 +1839,7 @@ bool Game::loadSceneXML(std::string scene)
 				ElementDef& d = elemsDefs[i];
 				if(d.repeat)
 				{
-					if(!(is >> d.rsx >> d.rsx))
+					if(!(is >> d.rsx >> d.rsy))
 						break;
 				}
 			}
@@ -1854,6 +1855,14 @@ bool Game::loadSceneXML(std::string scene)
 			}
 		}
 		simpleElements = simpleElements->NextSiblingElement("SE");
+	}
+
+	if(fullTilesetReload)
+	{
+		fullTilesetReload = false;
+		// used by SceneEditor
+		// no elements exist right now -> textures will be cleared and reloaded
+		dsq->texmgr.clearUnused();
 	}
 
 	// figure out which textures in the tileset are used and preload those that are actually used
@@ -1904,8 +1913,7 @@ bool Game::loadSceneXML(std::string scene)
 		e->rotation.z = d.rot;
 		e->repeatToFillScale.x = d.rsx;
 		e->repeatToFillScale.y = d.rsy;
-		if(d.efxIdx)
-			e->setElementEffectByIndex(d.efxIdx);
+		e->setElementEffectByIndex(d.efxIdx);
 		if (d.repeat)
 			e->repeatTextureToFill(true); // also applies repeatToFillScale
 		e->setTag(d.tag);

Aquaria/Game.h
@@ -350,6 +350,7 @@ public:
 	bool loadingScene;
 	bool doScreenTrans;
 	bool noSceneTransitionFadeout;
+	bool fullTilesetReload;
 
 	WaterSurfaceRender *waterSurfaceRender;
 

Aquaria/SceneEditor.cpp
@@ -1817,6 +1817,7 @@ void SceneEditor::loadSceneByName()
 	if (!s.empty())
 	{
 		game->noSceneTransitionFadeout = true;
+		game->fullTilesetReload = true;
 		game->transitionToScene(s);
 	}
 }
@@ -1825,6 +1826,7 @@ void SceneEditor::reloadScene()
 {
 	debugLog("reloadScene");
 	game->noSceneTransitionFadeout = true;
+	game->fullTilesetReload = true;
 	game->positionToAvatar = game->avatar->position;
 	game->transitionToScene(game->sceneName);
 }

BBGE/TextureMgr.cpp
@@ -8,13 +8,14 @@
 
 struct TexLoadTmp
 {
-    TexLoadTmp() : loadmode(TextureMgr::KEEP), curTex(NULL), success(false) { img.pixels = NULL; }
+    TexLoadTmp() : loadmode(TextureMgr::KEEP), curTex(NULL), success(false), arrayidx(0) { img.pixels = NULL; }
     std::string name, filename;
     ImageData img;
     TextureMgr::LoadMode loadmode;
     Texture *curTex; // immutable
     bool success; // if this is true and img.pixels is NULL, don't change anything
     //bool mipmap;
+    size_t arrayidx;
 };
 
 typedef ImageData (*ImageLoadFunc)(const char *fn);
@@ -289,6 +290,7 @@ void TextureMgr::loadBatch(Texture * pdst[], const std::string texnames[], size_
     for(size_t i = 0; i < n; ++i)
     {
         TexLoadTmp& tt = tmp[i];
+        tt.arrayidx = i;
         tt.name = texnames[i];
         stringToLower(tt.name);
         TexCache::iterator it = cache.find(tt.name);
@@ -315,7 +317,7 @@ void TextureMgr::loadBatch(Texture * pdst[], const std::string texnames[], size_
         TexLoadTmp& tt = *(TexLoadTmp*)p;
         Texture *tex = finalize(tt);
         if(pdst)
-            pdst[i] = tex;
+            pdst[tt.arrayidx] = tex;
         if(cb)
             cb(++doneCB, cbUD);
     }