AquariaOSE/Aquaria

commit 09edbf49fd09b95d2b87aa430c21af01eab93857

Changes to rendering logic, incl water surface flicker fix, plus other fixes.
This commit changes a bunch of internal rendering logic to
use FBOs in a way that doesn't violate the GL spec.
The water surface FBO's output texture was bound for reading
while at the same time rendering the water surface back into the
same FBO! Depending on the card/driver/load/zoom factor/moon phase,
this could lead to water surface flickering, chessboard effects,
and other visual glitches.
In order to fix this an extra FBO is needed.
In theory this is a simple fix but in practice this is the Aquaria
codebase and everything is more complicated than it has any right to be.

Couple other things:

- FBOs no longer have a depth renderbuffer. Aquaria never uses the depth
buffer for anything, so this can go to save some memory.
Also remove renderbuffer GL function pointers.

- Make FBOs multi-"paged". This is supposedly more efficient on desktop GL,
if glDrawBuffer() supports GL_COLOR_ATTACHMENTn. This is currently checked
via the presence of glDrawBuffersARB().

- Main core FBO now has 2 pages becaus it's needed for the water surface.
The same 2 pages are later used by the after effect manager to ping-pong
postprocessing shaders. Remove private after effect FBO.

TODO:
- There's still a bug in the one-fbo-multiple-binding-points code path.
-> for now glDrawBuffersARB must be NULL to work properly.

Aquaria/DSQ.cpp
@@ -201,7 +201,6 @@ DSQ::DSQ(const std::string& fileSystem, const std::string& extraDataDir)
 	console = 0;
 	cmDebug = 0;
 	saveSlotMode = SSM_NONE;
-	afterEffectManagerLayer = LR_AFTER_EFFECTS; // LR_AFTER_EFFECTS
 	renderObjectLayers.resize(LR_MAX);
 
 	entities.resize(64, 0);
@@ -585,7 +584,7 @@ static void loadBit(int index, float perc = 1)
 
 	loading->setWidthHeight(loadingProgress*600, 23);
 
-	dsq->render();
+	dsq->renderExternal();
 	dsq->showBuffer();
 }
 
@@ -687,6 +686,45 @@ static bool sdlVideoModeOK(int disp, const int w, const int h, const int bpp)
 	return SDL_VideoModeOK(w, h, bpp, SDL_OPENGL | SDL_FULLSCREEN);
 #endif
 }
+
+static bool preRenderDarkLayer(const RenderState& rs)
+{
+	if(core->darkLayer.isUsed())
+		core->darkLayer.beginCapture();
+
+	return true;
+}
+
+static void postRenderDarkLayer(const RenderState& rs)
+{
+	if(core->darkLayer.isUsed())
+	{
+		core->darkLayer.endCapture();
+	}
+}
+
+static bool beginAfterEffectCapture(const RenderState& rs)
+{
+	if (core->afterEffectManager)
+		core->afterEffectManager->beginCapture();
+
+	return true;
+}
+
+static bool preRenderAfterEffectLayer(const RenderState& rs)
+{
+	if(core->darkLayer.isUsed())
+		core->darkLayer.render(rs);
+
+	if(core->afterEffectManager)
+	{
+		unsigned page = core->frameBuffer.popCapture();
+		core->afterEffectManager->render(rs, page);
+	}
+
+	return true;
+}
+
 
 void DSQ::init()
 {
@@ -911,12 +949,6 @@ void DSQ::init()
 	precacher.clear();
 
 
-
-	render();
-	showBuffer();
-
-
-
 	loadBit(LOAD_INITIAL);
 
 	debugLog("Loading Particle Bank...");
@@ -968,7 +1000,6 @@ void DSQ::init()
 
 	user.video.darkbuffersize = MAX(user.video.darkbuffersize,128);
 
-	darkLayer.setLayers(LR_ELEMENTS13, LR_AFTER_EFFECTS);
 	debugLog("dark layer init");
 	darkLayer.init(user.video.darkbuffersize, user.video.darkfbuffer);
 	debugLog("dark layer togle...");
@@ -1269,6 +1300,14 @@ void DSQ::init()
 	renderObjectLayerOrder[LR_ENTITIES_MINUS4] = -1;
 	renderObjectLayerOrder[LR_ENTITIES_MINUS3] = -1;
 	renderObjectLayerOrder[LR_ENTITIES_MINUS2] = -1;
+
+	renderObjectLayers[LR_ZERO].preRender = beginAfterEffectCapture;
+	renderObjectLayers[LR_AFTER_EFFECTS].preRender = preRenderAfterEffectLayer;
+
+	renderObjectLayers[LR_ELEMENTS13].preRender = preRenderDarkLayer;
+	renderObjectLayers[LR_ELEMENTS13].postRender = postRenderDarkLayer;
+	darkLayer.beginLayer = LR_ELEMENTS13;
+	darkLayer.lastLayer = LR_ELEMENTS13;
 
 
 
@@ -2717,8 +2756,8 @@ void DSQ::doSaveSlotMenu(SaveSlotMode ssm, const Vector &position)
 
 		glPushAttrib(GL_VIEWPORT_BIT);
 		glViewport(0, 0, renderWidth, renderHeight);
-		clearBuffers();
-		render();
+		glClear(GL_COLOR_BUFFER_BIT);
+		renderExternal();
 		scrShotData = grabScreenshot(x, y, scrShotWidth, scrShotHeight);
 		glPopAttrib();
 		showBuffer();
@@ -3315,6 +3354,11 @@ void DSQ::prepScreen(bool screenshot)
 	}
 }
 
+void DSQ::onPrepareRender()
+{
+	game->onPrepareRender();
+}
+
 void DSQ::onRender()
 {
 	if (cursor)
@@ -3661,7 +3705,7 @@ void DSQ::onUpdate(float dt)
 				pollEvents(sec);
 				ActionMapper::onUpdate(sec);
 				SDL_Delay(int(sec*1000));
-				render();
+				renderExternal();
 				showBuffer();
 				resetTimer();
 
@@ -4162,17 +4206,18 @@ void AquariaScreenTransition::capture()
 	InterpolatedVector oldAlpha = dsq->cursor->alpha;
 	dsq->cursor->alpha.x = 0;
 	int width=0, height=0;
-	dsq->render();
+	dsq->renderExternal();
 
 	width = dsq->getWindowWidth();
 	height = dsq->getWindowHeight();
 
 	glBindTexture(GL_TEXTURE_2D,screen_texture);
+	glReadBuffer(GL_BACK);
 	glCopyTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, 0, 0, width, height);
 
 
 	dsq->cursor->alpha = oldAlpha;
-	dsq->render();
+	dsq->renderExternal();
 	dsq->showBuffer();
 
 	this->alpha = 1;

Aquaria/DSQ.h
@@ -444,8 +444,9 @@ protected:
 	float shakeCameraTimer;
 	float shakeCameraMag;
 
-	void onUpdate(float dt);
-	void onRender();
+	void onUpdate(float dt) OVERRIDE;
+	void onRender() OVERRIDE;
+	void onPrepareRender() OVERRIDE;
 
 	void modifyDt(float &dt);
 

Aquaria/Game.cpp
@@ -232,6 +232,7 @@ Game::Game() : StateObject()
 	noSceneTransitionFadeout = false;
 	fullTilesetReload = false;
 	highestLoadedEntityID = 0;
+	waterSurfaceRender = NULL;
 }
 
 Game::~Game()
@@ -2032,7 +2033,7 @@ bool Game::saveScene(std::string scene)
 		saveFile.InsertEndChild(pathXml);
 	}
 
-	for(size_t lr = 0; lr < MAX_TILE_LAYERS; ++lr)
+	for(unsigned lr = 0; lr < MAX_TILE_LAYERS; ++lr)
 	{
 		const TileStorage& ts = dsq->tilemgr.tilestore[lr];
 		std::ostringstream simpleElements;
@@ -2084,7 +2085,7 @@ bool Game::saveScene(std::string scene)
 		{
 			XMLElement *simpleElementsXML = saveFile.NewElement("SE");
 			simpleElementsXML->SetAttribute("k", s.c_str());
-			simpleElementsXML->SetAttribute("l", (unsigned)lr);
+			simpleElementsXML->SetAttribute("l", lr);
 			std::string str = simpleElements_repeatScale.str();
 			if(!str.empty())
 				simpleElementsXML->SetAttribute("repeatScale", str.c_str());
@@ -2436,6 +2437,7 @@ void Game::applyState()
 	inHelpScreen = false;
 	helpBG = 0;
 	helpBG2 = 0;
+	waterSurfaceRender = NULL;
 
 	dsq->returnToScene = "";
 
@@ -4788,6 +4790,7 @@ void Game::removeState()
 	gridRenderUser1 = 0;
 	gridRenderUser2 = 0;
 	worldMapRender = 0;
+	waterSurfaceRender = 0;
 
 	clearObsRows();
 
@@ -4986,4 +4989,10 @@ bool Game::isIgnoreAction(AquariaActions ac) const
 void Game::onContinuityReset()
 {
 	themenu->onContinuityReset();
+}
+
+void Game::onPrepareRender()
+{
+	if(waterSurfaceRender)
+		waterSurfaceRender->prepareRender();
 }

Aquaria/Game.h
@@ -398,6 +398,7 @@ public:
 	bool isIgnoreAction(AquariaActions ac) const;
 
 	void onContinuityReset();
+	void onPrepareRender();
 
 protected:
 	unsigned highestLoadedEntityID;

Aquaria/WaterSurfaceRender.cpp
@@ -6,30 +6,26 @@
 #include "RenderBase.h"
 
 
-
 WaterSurfaceRender::WaterSurfaceRender() : Quad()
 {
 	cull = false;
 	this->texcoordOverride = true;
 
-	if (dsq->useFrameBuffer && dsq->frameBuffer.isInited())
+	if (dsq->useFrameBuffer)
 	{
 		setSegs(4, 32, 0.5f, 0.5f, -0.005f, 0, 5, 1);
 	}
 
-
+	this->renderBorder = false;
+	this->renderCenter = false;
+	this->borderAlpha = 1;
 
 	qLine = new Quad("water/water-line", Vector(0,0));
 	qLine->repeatTextureToFill(1);
 	qLine->cull = false;
 	game->addRenderObject(qLine, LR_WATERSURFACE2);
 
-	qLine2 = 0;
-
-
-
 	qSurface = new Quad("missingimage", Vector(0,0));
-
 	qSurface->cull = false;
 	qSurface->repeatTextureToFill(1);
 	game->addRenderObject(qSurface, LR_WATERSURFACE2);
@@ -38,15 +34,27 @@ WaterSurfaceRender::WaterSurfaceRender() : Quad()
 	shareAlphaWithChildren = 0;
 }
 
-void WaterSurfaceRender::onUpdate(float dt)
+WaterSurfaceRender::~WaterSurfaceRender()
 {
+}
+
+void WaterSurfaceRender::onEndOfLife()
+{
+	if(qLine)
+		qLine->safeKill();
+	if(qSurface)
+		qSurface->safeKill();
+
+	qLine = NULL;
+	qSurface = NULL;
+}
+
+void WaterSurfaceRender::prepareRender()
+{
+	bool fbEffectVisible = false;
 	if (game->waterLevel.x > 0)
 	{
-		Quad::onUpdate(dt);
-
 		qLine->alpha = qSurface->alpha = 1;
-
-
 
 		position.x = core->screenCenter.x;
 		position.y = game->waterLevel.x;
@@ -88,28 +96,24 @@ void WaterSurfaceRender::onUpdate(float dt)
 
 		if (dsq->useFrameBuffer && dsq->frameBuffer.isInited())
 		{
-			qSurface->alphaMod = 0.5;
+			qSurface->alphaMod = 0.5f;
+			fbEffectVisible = this->isOnScreen();
 		}
 	}
 	else
 	{
 		qLine->alpha = qSurface->alpha = 0;
-
-		if (qLine2)
-		{
-			qLine2->alpha = 0;
-		}
 	}
 
-	if (dsq->useFrameBuffer && dsq->frameBuffer.isInited())
+	if (fbEffectVisible)
 	{
 		const float reflectSize = 97;
 		const float reflectPos = (game->waterLevel.x - core->cameraPos.y)
 			+ (game->waterLevel.x - core->screenCenter.y) / 3;
 		const float reflectOffset = -0.03f;
-		const float coordDiv = 768;
-		const float v0 = 1 + reflectOffset - (reflectPos * core->globalScale.x) / coordDiv;
-		const float v1 = v0 + (reflectSize * core->globalScale.x) / coordDiv;
+		const float coordMul = 1.0f / 768;
+		const float v0 = 1 + reflectOffset - (reflectPos * core->globalScale.x) * coordMul;
+		const float v1 = v0 + (reflectSize * core->globalScale.x) * coordMul;
 
 		texcoords.u1 = 0;
 		texcoords.u2 = core->frameBuffer.getWidthP();
@@ -127,27 +131,73 @@ void WaterSurfaceRender::onUpdate(float dt)
 		color = Vector(0.4f, 0.7f, 0.8f);
 		alpha = 0.2f;
 	}
+	//debugLog(fbEffectVisible ? "ws on screen" : "ws not vis");
+	this->fbEffectVisible = fbEffectVisible;
+	this->renderBorder = RenderObject::renderCollisionShape;
+	if(this->renderBorder)
+	{
+		this->renderBorderColor = fbEffectVisible ? Vector(1,0,1) : Vector(0,0.5f,1);
+	}
 
 	grid->setTexCoords(texcoords);
+}
+
+static void quadBlit(const RenderState& rs, unsigned tex)
+{
+	rs.gpu.setBlend(BLEND_DISABLED);
+	glBindTexture(GL_TEXTURE_2D, tex);
+	glColor4f(1,1,1,1);
+
+	glPushMatrix();
+	glLoadIdentity();
+	glScalef(core->globalResolutionScale.x, core->globalResolutionScale.y, core->globalResolutionScale.z);
+
+	int vw = core->getVirtualWidth();
+	int vh = core->getVirtualHeight();
+	int offx = -core->getVirtualOffX();
+	int offy = -core->getVirtualOffY();
+
+	// verts are in 0..1, transform so that we cover the entire screen
+	glTranslatef(offx, offy, 0);
+	glScalef(vw, vh, 1);
+
+	core->blitQuad.render(rs);
+	glPopMatrix();
 }
 
 void WaterSurfaceRender::render(const RenderState& rs) const
 {
 	if (game->waterLevel.x > 0)
+	{
+		if (fbEffectVisible)
+		{
+			// Everything up until now was rendered to the first page; flip it
+			const unsigned curpage = core->frameBuffer.getCurrentPage();
+			const unsigned oldtex = core->frameBuffer.getTextureID(curpage);
+			const unsigned newtex = core->frameBuffer.getTextureID(curpage + 1);
+
+			core->frameBuffer.replaceCapture(curpage + 1);
+
+			if(glCopyImageSubDataEXT)
+				glCopyImageSubDataEXT(
+					oldtex, GL_TEXTURE_2D, 0, 0, 0, 0,
+					newtex, GL_TEXTURE_2D, 0, 0, 0, 0,
+					core->width, core->height, 1
+				);
+			else
+				quadBlit(rs, oldtex);
+		}
+
 		Quad::render(rs);
+	}
 }
 
 void WaterSurfaceRender::onRender(const RenderState& rs) const
 {
-	if (game->waterLevel == 0) return;
-	if (dsq->useFrameBuffer && dsq->frameBuffer.isInited())
-	{
-		dsq->frameBuffer.bindTexture();
-	}
-	else
-	{
-		glBindTexture(GL_TEXTURE_2D, 0);
-	}
+	if (!fbEffectVisible || game->waterLevel == 0) return;
+
+
+	core->frameBuffer.bindTexture(core->frameBuffer.getCurrentPage() - 1);
 
 	Quad::onRender(rs);
 

Aquaria/WaterSurfaceRender.h
@@ -27,11 +27,14 @@ class WaterSurfaceRender : public Quad
 {
 public:
 	WaterSurfaceRender();
+	virtual ~WaterSurfaceRender();
 	void render(const RenderState& rs) const OVERRIDE;
+	void prepareRender();
 protected:
-	Quad *qSurface, *qLine, *qLine2;
+	virtual void onEndOfLife() OVERRIDE;
+	Quad *qSurface, *qLine;
 	void onRender(const RenderState& rs) const OVERRIDE;
-	void onUpdate(float dt) OVERRIDE;
+	bool fbEffectVisible;
 };
 
 #endif

BBGE/AfterEffect.cpp
@@ -72,6 +72,19 @@ void AfterEffectManager::deleteEffects()
 	openSpots.clear();
 }
 
+void AfterEffectManager::beginCapture()
+{
+	assert(core->frameBuffer.isInited());
+	core->frameBuffer.pushCapture(0);
+	glClearColor(0,0,0,0);
+	glClear(GL_COLOR_BUFFER_BIT);
+}
+
+void AfterEffectManager::endCapture()
+{
+	core->frameBuffer.popCapture();
+}
+
 void AfterEffectManager::deleteShaders()
 {
 	for(size_t i = 0; i < shaderPipeline.size(); ++i)
@@ -146,7 +159,7 @@ void AfterEffectManager::destroyEffect(int id)
 	openSpots.push_back(id);
 }
 
-void AfterEffectManager::render(const RenderState& rs) const
+void AfterEffectManager::render(const RenderState& rs, unsigned fboPageWithImage) const
 {
 	assert(core->frameBuffer.isInited());
 
@@ -154,48 +167,53 @@ void AfterEffectManager::render(const RenderState& rs) const
 
 	rs.gpu.setBlend(BLEND_DISABLED);
 
-	core->frameBuffer.endCapture();
 	glTranslatef(core->cameraPos.x, core->cameraPos.y, 0);
 	glScalef(core->invGlobalScale, core->invGlobalScale,0);
 
 	glColor4f(1,1,1,1);
-	renderGrid(rs);
+	renderGrid(rs, fboPageWithImage);
 
 	glPopMatrix();
 }
 
-void AfterEffectManager::renderGrid(const RenderState& rs) const
+void AfterEffectManager::renderGrid(const RenderState& rs, unsigned fbPage) const
 {
-	int firstShader = -1;
-	int lastShader = -1;
-	Shader *activeShader = 0;
+	size_t firstShader, lastShader;
+	Shader *activeShader = NULL;
 	for (size_t i = 0; i < shaderPipeline.size(); ++i)
 	{
 		if(shaderPipeline[i] && shaderPipeline[i]->isLoaded())
 		{
-			if(firstShader < 0)
+			if(!activeShader)
 			{
-				firstShader = int(i);
+				firstShader = i;
 				activeShader = shaderPipeline[i];
 			}
-			lastShader = int(i);
+			lastShader = i;
 		}
 	}
+
+	// Disable blending so we don't need to clear the framebuffers
+	rs.gpu.setBlend(BLEND_DISABLED);
 
 	int vw = core->getVirtualWidth();
 	int vh = core->getVirtualHeight();
 	int offx = -core->getVirtualOffX();
 	int offy = -core->getVirtualOffY();
 
-	core->frameBuffer.bindTexture();
+	const FrameBuffer * const fb = &core->frameBuffer;
+
+	// STARTING POINT: game image was just rendered into fb(0), use that as starting point
+	fb->bindTexture(fbPage);
 
 	if(activeShader)
 	{
-		activeShader->bind();
 		activeShader->setInt("tex", 0);
+		activeShader->bind();
 
+		// Unless this is the last pass, render to fb(1)
 		if(firstShader != lastShader)
-			backupBuffer.startCapture();
+			fb->pushCapture(1 - fbPage);
 	}
 
 	// verts are in 0..1, transform so that we cover the entire screen
@@ -208,39 +226,44 @@ void AfterEffectManager::renderGrid(const RenderState& rs) const
 		//renderGridPoints(rs);
 	}
 	else
-		blitQuad.render(rs);
+		core->blitQuad.render(rs);
 
 	if (activeShader)
+	{
 		activeShader->unbind();
 
-	if(firstShader != lastShader)
-	{
-		// From here on: secondary shader passes.
-		// We just outputted to the backup buffer...
-		const FrameBuffer *fbIn = &core->frameBuffer;
-		const FrameBuffer *fbOut = &backupBuffer;
-
-		for(int i = firstShader + 1; i <= lastShader; ++i)
+		if(firstShader != lastShader)
 		{
-			activeShader = shaderPipeline[i];
-			if(!(activeShader && activeShader->isLoaded()))
-				continue;
+			// From here on: secondary shader passes.
+			// We just outputted to the backup buffer...
 
-			// Swap and exchange framebuffers. The old output buffer serves as texture input for the other one
-			fbOut->endCapture();
-			std::swap(fbIn, fbOut);
-			fbIn->bindTexture();
+			unsigned pageOut = 1 - fbPage;
 
-			// If this is the last pass, do not render to a frame buffer again
-			if(i != lastShader)
-				fbOut->startCapture();
+			for(size_t i = firstShader + 1; i <= lastShader; ++i)
+			{
+				unsigned pageIn = 1 - pageOut;
+				activeShader = shaderPipeline[i];
+				if(!(activeShader && activeShader->isLoaded()))
+					continue;
 
-			activeShader->bind();
-			activeShader->setInt("tex", 0);
+				// Swap and exchange framebuffers. The old output buffer serves as texture input for the other one
+				pageOut = pageIn;
 
-			blitQuad.render(rs);
+				// If this is the last pass, do not render to a frame buffer again
+				if(i == lastShader)
+					fb->popCapture();
+				else
+					fb->replaceCapture(pageOut);
 
-			activeShader->unbind();
+				fb->bindTexture(pageIn);
+
+				activeShader->bind();
+				activeShader->setInt("tex", 0);
+
+				core->blitQuad.render(rs);
+
+				activeShader->unbind();
+			}
 		}
 	}
 
@@ -255,9 +278,7 @@ void AfterEffectManager::renderGridPoints(const RenderState& rs) const
 
 void AfterEffectManager::unloadDevice()
 {
-	backupBuffer.unloadDevice();
 	grid.dropBuffers();
-	blitQuad.dropBuffers();
 	unloadShaders();
 }
 
@@ -266,30 +287,12 @@ void AfterEffectManager::_updateScreenSize()
 	screenWidth = core->getWindowWidth();
 	screenHeight = core->getWindowHeight();
 
-	if (core->frameBuffer.isInited())
-	{
-		textureWidth = core->frameBuffer.getTexWidth();
-		textureHeight = core->frameBuffer.getTexHeight();
-	}
-	else
-	{
-		textureWidth = screenWidth;
-		sizePowerOf2Texture(textureWidth);
-		textureHeight = screenHeight;
-		sizePowerOf2Texture(textureHeight);
-	}
-
-	const float percentX = (float)screenWidth/(float)textureWidth;
-	const float percentY = (float)screenHeight/(float)textureHeight;
-	TexCoordBox tc = { 0, percentY, percentX, 0 }; // Y is upside down
-	grid.setTexCoords(tc);
-	blitQuad.setTexCoords(tc);
+	grid.setTexCoords(core->blitQuad.getTexCoords());
 }
 
 void AfterEffectManager::updateDevice()
 {
 	_updateScreenSize();
-	backupBuffer.init(-1, -1, true);
 	_initGrid();
 }
 
@@ -297,7 +300,6 @@ void AfterEffectManager::reloadDevice()
 {
 	_updateScreenSize();
 
-	backupBuffer.reloadDevice();
 	_initGrid();
 
 	for (size_t i = 0; i < loadedShaders.size(); ++i)
@@ -442,10 +444,6 @@ void AfterEffectManager::_initGrid()
 		grid.init(xDivs, yDivs);
 	else
 		grid.dropBuffers();
-
-	blitQuad.init(2, 2);
-	blitQuad.reset01();
-	blitQuad.updateVBO(); // never changed afterwards
 }
 
 void AfterEffectManager::deleteShader(int handle)

BBGE/AfterEffect.h
@@ -78,10 +78,13 @@ public:
 	void clear();
 	void deleteEffects();
 
+	void beginCapture();
+	void endCapture();
+
 	void resetGrid();
 
-	void render(const RenderState& rs) const;
-	void renderGrid(const RenderState& rs) const;
+	void render(const RenderState& rs, unsigned fboPageWithImage) const;
+	void renderGrid(const RenderState& rs, unsigned fbPage) const;
 	void renderGridPoints(const RenderState& rs) const;
 
 	void loadShaders();
@@ -105,17 +108,15 @@ protected:
 	int _insertShader(Shader *sh);
 	void _initGrid();
 
-	RenderGrid grid, blitQuad;
+	RenderGrid grid;
 	bool active;
 	int numEffects;
 	int xDivs, yDivs;
 	int screenWidth, screenHeight;
-	int textureWidth, textureHeight;
 	std::vector<Effect*> effects;
 	std::vector<int> openSpots;
 	std::vector<Shader*> shaderPipeline; // Shaders are applied in this order. Can contain the same pointer more than once.
 	std::vector<Shader*> loadedShaders;
-	FrameBuffer backupBuffer;
 
 };
 

commit 0f98893f288f55436702c6dc53d941e54169f9c9

Don't render the dark layer when its corresponding tile layer is hidden.
This would have the old (as it was last updated when visible) dark layer
stuck on the screen.

Regression from 09edbf49fd09b95d2

Aquaria/DSQ.cpp
@@ -713,8 +713,14 @@ static bool beginAfterEffectCapture(const RenderState& rs)
 
 static bool preRenderAfterEffectLayer(const RenderState& rs)
 {
-	if(core->darkLayer.isUsed())
+	if(core->darkLayer.isUsed()
+		// LR_DARK_LAYER is remapped to LR_ELEMENTS13 via renderLayerOrder,
+		// need to actually check LR_DARK_LAYER here
+		&& core->getRenderObjectLayer(LR_DARK_LAYER)->visible
+		)
+	{
 		core->darkLayer.render(rs);
+	}
 
 	if(core->afterEffectManager)
 	{