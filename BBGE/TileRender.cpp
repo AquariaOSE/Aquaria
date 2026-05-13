AquariaOSE/Aquaria

commit eb02f63c408e8d69633da9ff0bdb0f6db286c5fe

fix tex wrap behavior edge case
Turns out setting border mode to clamp is important to
prevent accidental wrapping behavior by some gfx drivers,
probably related to float inaccuracies.
In the vast majority of cases this was not noticeable
but there is one texture on the title screen where this
lead to a thin horizontal line on the screen.

thx timemaombro for reporting

BBGE/TileRender.cpp
@@ -124,7 +124,7 @@ void TileRender::onRender(const RenderState& rs) const
 			if(texid != lastTexId)
 			{
 				lastTexId = texid;
-				tex->apply();
+				tex->apply((tile.flags & TILEFLAG_REPEAT) ? TEX_BORDER_WRAP : TEX_BORDER_CLAMP);
 			}
 		}
 		else

Aquaria/MiniMapRender.cpp
@@ -477,7 +477,7 @@ void MiniMapRender::onRender(const RenderState& rs) const
 
 	if (alphaValue > 0)
 	{
-		texMinimapBtm->apply();
+		texMinimapBtm->apply(TEX_BORDER_CLAMP);
 
 		glBegin(GL_QUADS);
 			glColor4f(lightLevel, lightLevel, lightLevel, 1);
@@ -493,7 +493,7 @@ void MiniMapRender::onRender(const RenderState& rs) const
 
 		if (lightLevel > 0)
 		{
-			texWaterBit->apply();
+			texWaterBit->apply(TEX_BORDER_CLAMP);
 
 			rs.gpu.setBlend(BLEND_ADD);
 			glColor4f(0.1f, 0.2f, 0.9f, 0.4f*lightLevel);
@@ -615,7 +615,7 @@ void MiniMapRender::onRender(const RenderState& rs) const
 	glColor4f(1,1,1, alphaValue);
 
 	const int hsz = 20;
-	texNaija->apply();
+	texNaija->apply(TEX_BORDER_CLAMP);
 
 	glBegin(GL_QUADS);
 		glTexCoord2f(0, 1);
@@ -630,7 +630,7 @@ void MiniMapRender::onRender(const RenderState& rs) const
 
 	glColor4f(1,1,1,1);
 
-	texMinimapTop->apply();
+	texMinimapTop->apply(TEX_BORDER_CLAMP);
 	glBegin(GL_QUADS);
 		glTexCoord2f(0, 1);
 		glVertex2f(-miniMapGuiSize, miniMapGuiSize);
@@ -656,7 +656,7 @@ void MiniMapRender::onRender(const RenderState& rs) const
 		healthBarColor.normalize2D();
 	}
 
-	texHealthBar->apply();
+	texHealthBar->apply(TEX_BORDER_CLAMP);
 
 	rs.gpu.setBlend(BLEND_DEFAULT);
 	glColor4f(healthBarColor.x, healthBarColor.y, healthBarColor.z, 0.6f);
@@ -713,7 +713,7 @@ void MiniMapRender::onRender(const RenderState& rs) const
 	rs.gpu.setBlend(BLEND_DEFAULT);
 	glColor4f(1,1,1,1);
 
-	texMarker->apply();
+	texMarker->apply(TEX_BORDER_CLAMP);
 
 	const float x = healthLookupX[maxHealthSteps];
 	const float y = healthLookupY[maxHealthSteps];
@@ -751,7 +751,7 @@ void MiniMapRender::renderIcon(const MinimapIcon *ico, const Vector& pos) const
 	 	}
 	}
 
-	ico->tex->apply();
+	ico->tex->apply(TEX_BORDER_CLAMP);
 	const Vector c = ico->color;
 	const float a = ico->alpha.x * this->alpha.x;
 	glColor4f(c.x, c.y, c.z, a);

BBGE/BitmapFont.cpp
@@ -248,7 +248,7 @@ void BitmapText::onRender(const RenderState& rs) const
 	const glfont::GLFont * const font = bmpFont.font;
 	font->Begin();
 
-	if (bmpFont.overrideTexture) bmpFont.overrideTexture->apply();
+	if (bmpFont.overrideTexture) bmpFont.overrideTexture->apply(TEX_BORDER_CLAMP);
 
 	const float scale = bmpFont.scale;
 	float y=0;

BBGE/Emitter.cpp
@@ -260,7 +260,7 @@ void Emitter::onRender(const RenderState& rs) const
 	}
 
 	if (texture)
-		texture->apply();
+		texture->apply(TEX_BORDER_CLAMP);
 
 	Vector colorMult = rs.color;
 	if(data.inheritColor)

BBGE/RenderObject.cpp
@@ -493,7 +493,7 @@ nofollow:
 		{
 			if (texture->gltexid != lastTextureApplied)
 			{
-				texture->apply();
+				texture->apply(repeatTexture ? TEX_BORDER_WRAP : TEX_BORDER_CLAMP);
 				lastTextureApplied = texture->gltexid;
 			}
 		}

BBGE/Texture.cpp
@@ -47,6 +47,7 @@ Texture::Texture()
 {
 	gltexid = 0;
 	width = height = 0;
+	_bordermode = TEX_BORDER_CLAMP;
 
 	ow = oh = -1;
 	_mipmap = false;
@@ -134,9 +135,24 @@ void Texture::_freePixbuf()
 	}
 }
 
-void Texture::apply() const
+void Texture::apply(TexBorderMode border) const
 {
 	glBindTexture(GL_TEXTURE_2D, gltexid);
+	if(_bordermode != border)
+	{
+		_bordermode = border;
+		int mode = GL_CLAMP;
+		switch(border)
+		{
+		case TEX_BORDER_CLAMP:
+			break; // already this value
+		case TEX_BORDER_WRAP:
+			mode = GL_REPEAT;
+			break;
+		}
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, mode);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, mode);
+	}
 }
 
 struct GlTexFormat
@@ -169,8 +185,8 @@ bool Texture::upload(const ImageData& img, bool mipmap)
 	if(!gltexid)
 		glGenTextures(1, &gltexid);
 	glBindTexture(GL_TEXTURE_2D, gltexid);
-	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_S, GL_CLAMP);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_T, GL_CLAMP);
 
 	const GlTexFormat& f = formatLUT[img.channels - 1];
 

BBGE/Texture.h
@@ -48,6 +48,12 @@ enum TextureLoadResult
 	TEX_SUCCESS
 };
 
+enum TexBorderMode
+{
+	TEX_BORDER_CLAMP,
+	TEX_BORDER_WRAP
+};
+
 struct ImageData;
 
 class Texture : public Refcounted
@@ -56,7 +62,7 @@ public:
 	Texture();
 	~Texture();
 
-	void apply() const;
+	void apply(TexBorderMode border) const;
 	void unload();
 
 	unsigned gltexid;
@@ -78,6 +84,7 @@ protected:
 	void _freePixbuf();
 
 	int ow, oh;
+	mutable TexBorderMode _bordermode;
 	bool _mipmap;
 	mutable unsigned char *_pixbuf; // retrieved when needed
 };

commit 211e048555a0738537f5b518e3bddacdc35b11c8

Fix tile repeat flag not properly applied in some cases
Regression from eb02f63c408e8d6963

thx timemaombro for reporting

BBGE/TileRender.cpp
@@ -89,6 +89,7 @@ void TileRender::onRender(const RenderState& rs) const
 	const Vector T = C * (1 - F);
 
 	unsigned lastTexId = 0;
+	unsigned lastRepeatFlag = -1;
 
 	const bool renderExtras = renderBorders || RenderObject::renderCollisionShape;
 
@@ -121,10 +122,12 @@ void TileRender::onRender(const RenderState& rs) const
 		if(const Texture * const tex = et->tex.content())
 		{
 			unsigned texid = tex->gltexid;
-			if(texid != lastTexId)
+			unsigned repflag = tile.flags & TILEFLAG_REPEAT;
+			if(texid != lastTexId || repflag != lastRepeatFlag)
 			{
 				lastTexId = texid;
-				tex->apply((tile.flags & TILEFLAG_REPEAT) ? TEX_BORDER_WRAP : TEX_BORDER_CLAMP);
+				lastRepeatFlag = repflag;
+				tex->apply(repflag ? TEX_BORDER_WRAP : TEX_BORDER_CLAMP);
 			}
 		}
 		else